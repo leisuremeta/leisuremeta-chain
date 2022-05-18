e6654f3dbe1efc30848b8c7df2aaa2e5e8097aa4
sungkmi <sungkmi@gmail.com>
2022-05-18 16:48:27 +0900

    implement StateRepository

--- modules/lib/shared/src/main/scala/io/leisuremeta/chain/lib/merkle/MerkleTrieStateDiff.scala
+++ modules/lib/shared/src/main/scala/io/leisuremeta/chain/lib/merkle/MerkleTrieStateDiff.scala
@@ -5,36 +5,41 @@ import MerkleTrieNode.MerkleHash
 opaque type MerkleTrieStateDiff[K, V] =
   Map[MerkleHash[K, V], (MerkleTrieNode[K, V], Int)]
 
-extension [K, V](diff: MerkleTrieStateDiff[K, V])
-  def get(hash: MerkleHash[K, V]): Option[MerkleTrieNode[K, V]] =
-    diff.get(hash).flatMap {
-      case (node, count) if count > 0 => Some(node)
-      case _                          => None
-    }
-
-  def foreach(f: (MerkleHash[K, V], MerkleTrieNode[K, V]) => Unit): Unit =
-    for (hash, (node, count)) <- diff if count > 0
-    yield f(hash, node)
-    ()
-
-  def add(
-      hash: MerkleHash[K, V],
-      node: MerkleTrieNode[K, V],
-  ): MerkleTrieStateDiff[K, V] =
-    diff.get(hash).fold(diff + (hash -> (node, 1))) {
-      case (node, -1)    => diff - hash
-      case (node, count) => diff + (hash -> (node, count + 1))
-    }
-
-  def remove(
-      hash: MerkleHash[K, V],
-      node: MerkleTrieNode[K, V],
-  ): MerkleTrieStateDiff[K, V] =
-    diff.get(hash).fold(diff + (hash -> (node, -1))) {
-      case (node, count) if count == 1 => diff - hash
-      case (node, count)               => diff + (hash -> (node, count - 1))
-    }
-end extension
-
 object MerkleTrieStateDiff:
+
   def empty[K, V]: MerkleTrieStateDiff[K, V] = Map.empty
+
+  extension [K, V](diff: MerkleTrieStateDiff[K, V])
+    def get(hash: MerkleHash[K, V]): Option[MerkleTrieNode[K, V]] =
+      diff.get(hash).flatMap {
+        case (node, count) if count > 0 => Some(node)
+        case _                          => None
+      }
+
+    def foreach(f: (MerkleHash[K, V], MerkleTrieNode[K, V]) => Unit): Unit =
+      for (hash, (node, count)) <- diff if count > 0
+      yield f(hash, node)
+      ()
+
+    def add(
+        hash: MerkleHash[K, V],
+        node: MerkleTrieNode[K, V],
+    ): MerkleTrieStateDiff[K, V] =
+      diff.get(hash).fold(diff + (hash -> (node, 1))) {
+        case (node, -1)    => diff - hash
+        case (node, count) => diff + (hash -> (node, count + 1))
+      }
+
+    def remove(
+        hash: MerkleHash[K, V],
+        node: MerkleTrieNode[K, V],
+    ): MerkleTrieStateDiff[K, V] =
+      diff.get(hash).fold(diff + (hash -> (node, -1))) {
+        case (node, 1)     => diff - hash
+        case (node, count) => diff + (hash -> (node, count - 1))
+      }
+
+    def toList: List[(MerkleHash[K, V], (MerkleTrieNode[K, V], Int))] =
+      diff.toList
+  end extension
+
