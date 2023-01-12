294cac4bb5e9078838e2b029a532f1ba8c5d792e
G-yhlee <yhleesoft@gmail.com>
2023-01-12 10:29:32 +0900

    edit board style

--- modules/lmscan-frontend/src/main/scala/io/leisuremeta/chain/lmscan/frontend/components/@widgets/board/BoardView.scala
+++ modules/lmscan-frontend/src/main/scala/io/leisuremeta/chain/lmscan/frontend/components/@widgets/board/BoardView.scala
@@ -2,31 +2,37 @@ package io.leisuremeta.chain.lmscan.frontend
 import tyrian.Html.*
 import tyrian.*
 
+object Board:
+  val LM_Price     = "LM Price = 0.394 USDT"
+  val Block_Number = "Block Number 21,872,421"
+  val Transactions = "24h Transactions 3,572,245"
+  val Accounts     = "Total Accounts 194,142,552"
+
 object BoardView:
   def view(model: Model): Html[Msg] =
     div(`class` := "board-area")(
-      div(`class` := "board-list center")(
-        div(`class` := "board-container center")(
+      div(`class` := "board-list x")(
+        div(`class` := "board-container xy-center")(
           div(
-            `class` := "board-text center",
-          )("LM Price = 0.394 USDT"),
+            `class` := "board-text xy-center",
+          )(Board.LM_Price),
         ),
-        div(`class` := "board-container center")(
+        div(`class` := "board-container xy-center")(
           div(
-            `class` := "board-text center",
-          )("Block Number 21,872,421"),
+            `class` := "board-text xy-center",
+          )(Board.Block_Number),
         ),
       ),
-      div(`class` := "board-list center")(
-        div(`class` := "board-container center")(
+      div(`class` := "board-list x")(
+        div(`class` := "board-container xy-center")(
           div(
-            `class` := "board-text center",
-          )("24h Transactions 3,572,245"),
+            `class` := "board-text xy-center",
+          )(Board.Transactions),
         ),
-        div(`class` := "board-container center")(
+        div(`class` := "board-container xy-center")(
           div(
-            `class` := "board-text center",
-          )("Total Accounts 194,142,552"),
+            `class` := "board-text xy-center",
+          )(Board.Accounts),
         ),
       ),
     )
--- modules/lmscan-frontend/src/main/scala/io/leisuremeta/chain/lmscan/frontend/components/@widgets/search/SearchView.scala
+++ modules/lmscan-frontend/src/main/scala/io/leisuremeta/chain/lmscan/frontend/components/@widgets/search/SearchView.scala
@@ -4,18 +4,16 @@ import tyrian.*
 
 object SearchView:
   def view(model: Model): Html[Msg] =
-    div(
-      div(`class` := "search-area center")(
-        div(`class` := "search-container center")(
-          input(
-            `class` := "search-text center",
-            `placeholder` := (
-              "block number, block hash, account, tx hash",
-            ),
+    div(`class` := "search-area")(
+      div(`class` := "search-container xy-center")(
+        input(
+          `class` := "search-text xy-center",
+          `placeholder` := (
+            "block number, block hash, account, tx hash",
           ),
-          div(`class` := "search-icon center material-symbols-outlined")(
-            "search",
-          ),
+        ),
+        div(`class` := "search-icon xy-center material-symbols-outlined")(
+          "search",
         ),
       ),
     )
