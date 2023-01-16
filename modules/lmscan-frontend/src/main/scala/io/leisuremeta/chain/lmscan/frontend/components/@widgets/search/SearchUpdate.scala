7051af5fdccf82f79a71c757285ec3ca52e4c707
G-yhlee <yhleesoft@gmail.com>
2023-01-16 11:46:31 +0900

    add search match - case statement

--- modules/lmscan-frontend/src/main/scala/io/leisuremeta/chain/lmscan/frontend/components/@widgets/search/SearchUpdate.scala
+++ modules/lmscan-frontend/src/main/scala/io/leisuremeta/chain/lmscan/frontend/components/@widgets/search/SearchUpdate.scala
@@ -4,9 +4,23 @@ import cats.effect.IO
 import tyrian.Html.*
 import tyrian.*
 
+import Log.log
+
 object InputUpdate:
   def update(model: Model): InputMsg => (Model, Cmd[IO, Msg]) =
     case InputMsg.Get(s) =>
       (model.copy(searchValue = s), Cmd.None)
     case InputMsg.Patch =>
-      (model.copy(searchValue = ""), Cmd.None)
+      log("패치했을때의", model.searchValue)
+
+      val updated =
+        model.copy(
+          searchValue = "",
+          tab = model.searchValue match
+            case "1" => NavMsg.DashBoard
+            case "2" => NavMsg.Blocks
+            case "3" => NavMsg.Transactions
+            case _   => log("none"); NavMsg.DashBoard,
+        )
+
+      (updated, Cmd.None)
