534e1626809d8f273cfecc060625bbce10e532af
G-yhlee <yhleesoft@gmail.com>
2023-01-17 16:28:31 +0900

    add title

--- modules/lmscan-frontend/src/main/scala/io/leisuremeta/chain/lmscan/frontend/components/@pages/blocks/BlocksView.scala
+++ modules/lmscan-frontend/src/main/scala/io/leisuremeta/chain/lmscan/frontend/components/@pages/blocks/BlocksView.scala
@@ -5,6 +5,9 @@ import tyrian.*
 object BlocksView:
   def view(model: Model): Html[Msg] =
     div(`class` := "table-area")(
+      div(`class` := "font-40px pt-16px font-block-detail")(
+        "Blocks",
+      ),
       div(id := "oop-table-blocks", `class` := "table-list x")(
         TableBlockView.view(model),
       ),
--- modules/lmscan-frontend/src/main/scala/io/leisuremeta/chain/lmscan/frontend/components/@pages/transcations/TransactionsView.scala
+++ modules/lmscan-frontend/src/main/scala/io/leisuremeta/chain/lmscan/frontend/components/@pages/transcations/TransactionsView.scala
@@ -5,6 +5,9 @@ import tyrian.*
 object TransactionsView:
   def view(model: Model): Html[Msg] =
     div(`class` := "table-area")(
+      div(`class` := "font-40px pt-16px font-block-detail")(
+        "Transactions",
+      ),
       div(id := "oop-table-blocks", `class` := "table-list x")(
         TableTransactionsView.view(model),
       ),
