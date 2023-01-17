71f3fcd9633b231479d87ad5c5ec74abb042d31e
jichangho <jichangho20@gmail.com>
2023-01-17 10:08:09 +0900

    add TransactionService file

---
+++ modules/lmscan-backend/src/main/scala/io/leisuremeta/chain/lmscan/backend/service/TransactionService.scala
@@ -0,0 +1,25 @@
+package io.leisuremeta.chain.lmscan.backend.service
+
+import io.leisuremeta.chain.lmscan.backend.entity.Tx
+import io.leisuremeta.chain.lmscan.backend.model.PageNavigation
+import io.leisuremeta.chain.lmscan.backend.repository.TransactionRepository
+import cats.Functor
+import cats.data.EitherT
+import cats.Monad
+import eu.timepit.refined.boolean.False
+import cats.effect.Async
+import scala.concurrent.ExecutionContext
+
+object TransactionService:
+
+  // EitherT[F[_], A, B] is a lightweight wrapper for F[Either[A, B]]
+  // that makes it easy to compose Eithers and Fs together.
+  def getPage[F[_]: Async](
+      pageNavInfo: PageNavigation,
+  ): EitherT[F, String, Seq[Tx]] =
+    TransactionRepository.getPage(pageNavInfo)
+
+  def get[F[_]: Async](
+      hash: String,
+  )(using ExecutionContext): EitherT[F, String, Option[Tx]] =
+    TransactionRepository.get(hash)
