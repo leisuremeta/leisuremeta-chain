3e7e9afae99cc64296ae468eaf3bee3fa8ca6fb6
G-yhlee <yhleesoft@gmail.com>
2023-01-18 16:42:22 +0900

    add transcation inputout component

--- modules/lmscan-frontend/src/main/scala/io/leisuremeta/chain/lmscan/frontend/components/@pages/account/AccountView.scala
+++ modules/lmscan-frontend/src/main/scala/io/leisuremeta/chain/lmscan/frontend/components/@pages/account/AccountView.scala
@@ -9,10 +9,4 @@ object AccountView:
       div(`class` := "font-40px pt-16px font-block-detail pb-16px")(
         "AccountView",
       ),
-      div(`class` := "x")(CommonDetailTable.view(model)),
-      div(`class` := "table-area ")(
-        div(id := "oop-table-blocks", `class` := "table-list x")(
-          CommonTransactionTable.view(model),
-        ),
-      ),
     )
