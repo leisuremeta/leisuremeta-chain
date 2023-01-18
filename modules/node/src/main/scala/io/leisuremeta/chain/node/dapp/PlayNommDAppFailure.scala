d5e74916fefd500a1e415941c03a00482132bb0a
sungkmi <sungkmi@gmail.com>
2023-01-19 03:56:59 +0900

    define PlayNommDAppFailure

---
+++ modules/node/src/main/scala/io/leisuremeta/chain/node/dapp/PlayNommDAppFailure.scala
@@ -0,0 +1,33 @@
+package io.leisuremeta.chain.node.dapp
+
+import cats.{~>, Functor, Monad}
+import cats.arrow.FunctionK
+import cats.data.{EitherT, StateT}
+import cats.syntax.bifunctor.*
+
+sealed trait PlayNommDAppFailure:
+  def msg: String
+object PlayNommDAppFailure:
+  final case class External(msg: String) extends PlayNommDAppFailure
+  final case class Internal(msg: String) extends PlayNommDAppFailure
+
+  def external(msg: String): PlayNommDAppFailure = External(msg)
+  def internal(msg: String): PlayNommDAppFailure = Internal(msg)
+
+  def mapExternal[F[_]: Functor](
+      msg: String,
+  ): EitherT[F, String, *] ~> EitherT[F, PlayNommDAppFailure, *] =
+    FunctionK.lift {
+      [A] =>
+        (stringOr: EitherT[F, String, A]) =>
+          stringOr.leftMap(e => PlayNommDAppFailure.external(s"$msg: $e"))
+    }
+
+  def mapInternal[F[_]: Functor](
+      msg: String,
+  ): EitherT[F, String, *] ~> EitherT[F, PlayNommDAppFailure, *] =
+    FunctionK.lift {
+      [A] =>
+        (stringOr: EitherT[F, String, A]) =>
+          stringOr.leftMap(e => PlayNommDAppFailure.internal(s"$msg: $e"))
+    }
