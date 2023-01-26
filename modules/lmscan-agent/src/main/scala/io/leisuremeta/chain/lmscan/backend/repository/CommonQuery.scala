9937df255120a4b0ca36a79600c29172d1e3239d
jichangho <jichangho20@gmail.com>
2023-01-26 11:00:03 +0900

    init lmscan-agnet

---
+++ modules/lmscan-agent/src/main/scala/io/leisuremeta/chain/lmscan/backend/repository/CommonQuery.scala
@@ -0,0 +1,111 @@
+package io.leisuremeta.chain.lmscan.backend.repository
+
+import cats.data.EitherT
+import cats.effect.kernel.Async
+import io.getquill.Query
+import java.sql.SQLException
+import cats.implicits.*
+import cats.effect.{Async, IO}
+import io.getquill.PostgresJAsyncContext
+import io.getquill.SnakeCase
+import io.getquill.*
+import io.getquill.Literal
+
+import scala.concurrent.ExecutionContext.global
+import scala.concurrent.ExecutionContext
+
+trait CommonQuery:
+  val ctx = new PostgresJAsyncContext(SnakeCase, "ctx")
+  inline def seqQuery[F[_]: Async, T](
+      inline query: Query[T],
+  ): EitherT[F, String, Seq[T]] =
+    EitherT {
+      Async[F].recover {
+        for
+          given ExecutionContext <- Async[F].executionContext
+          result <- Async[F]
+            .fromFuture(Async[F].delay {
+              // scribe.info(s"Running page query...")
+              // type T = Seq[Transaction]
+              // inline val wrap = OuterSelectWrap.Default
+              // inline val q    = pagedQuery(lift(offset), lift(sizePerRequest))
+              // inline val quoted = q
+              // val ca =
+              //   io.getquill.context.ContextOperation
+              //     .Factory[
+              //       PostgresDialect,
+              //       LowerCase,
+              //       PrepareRow,
+              //       ResultRow,
+              //       Session,
+              //       ctx.type,
+              //     ](ctx.idiom, ctx.naming)
+              //     .op[Nothing, T, Result[RunQueryResult[T]]] { arg =>
+              //       val simpleExt = arg.extractor.requireSimple()
+              //       ctx.executeQuery(arg.sql, arg.prepare, simpleExt.extract)(
+              //         arg.executionInfo,
+              //         io.getquill.context
+              //           .DatasourceContextInjectionMacro[
+              //             RunnerBehavior,
+              //             Runner,
+              //             ctx.type,
+              //           ](context),
+              //       )
+              //     }
+              // QueryExecution.apply(ca)(quoted, None, wrap)
+              // InternalApi.runQuery(q, OuterSelectWrap.Default)
+              // InternalApi.runQueryDefault(q)
+              ctx.run(query)
+            })
+            .map(Either.right(_))
+        yield
+          scribe.info(s"Result: $result")
+          result
+      } {
+        case e: SQLException =>
+          Left(s"sql exception occured: " + e.getMessage())
+        case e: Exception => Left(e.getMessage())
+      }
+    }
+
+  inline def countQuery[F[_]: Async, T](
+      inline query: Query[T],
+  ): EitherT[F, String, Long] =
+    EitherT {
+      Async[F].recover {
+        for
+          given ExecutionContext <- Async[F].executionContext
+          result <- Async[F]
+            .fromFuture(Async[F].delay {
+              ctx.run(query.size)
+            })
+            .map(Either.right(_))
+        yield
+          scribe.info(s"Result: $result")
+          result
+      } {
+        case e: SQLException =>
+          Left(s"sql exception occured: " + e.getMessage())
+        case e: Exception => Left(e.getMessage())
+      }
+    }
+
+  inline def optionQuery[F[_]: Async, T](
+      inline query: Query[T],
+  ): EitherT[F, String, Option[T]] =
+    EitherT {
+      Async[F].recover {
+        for detail <- Async[F]
+            .fromFuture(Async[F].delay {
+              ctx.run(query)
+            })
+        yield Right(detail.headOption)
+      } {
+        case e: SQLException =>
+          Left(s"sql exception occured: " + e.getMessage())
+        case e: Exception => Left(e.getMessage())
+      }
+    }
+
+  def calTotalPage(totalCnt: Long, sizePerRequest: Integer): Integer =
+    Math.ceil(totalCnt.toDouble / sizePerRequest).toInt;
---
+++ modules/lmscan-agent/src/main/scala/io/leisuremeta/chain/lmscan/backend/repository/TransactionRepository.scala
@@ -0,0 +1,112 @@
+package io.leisuremeta.chain.lmscan.backend.repository
+
+import io.leisuremeta.chain.lmscan.backend.model.PageNavigation
+import io.leisuremeta.chain.lmscan.backend.model.PageResponse
+import io.leisuremeta.chain.lmscan.backend.repository.CommonQuery
+import io.leisuremeta.chain.lmscan.backend.entity.Tx
+import cats.data.EitherT
+import cats.implicits.*
+
+import io.getquill.*
+
+import cats.effect.{Async, IO}
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.global
+import java.sql.SQLException
+import scala.concurrent.ExecutionContext
+
+trait TransactionRepository[F[_]]:
+  def getPage(
+      pageNavInfo: PageNavigation,
+  ): EitherT[F, String, Seq[Tx]]
+
+object TransactionRepository extends CommonQuery:
+
+  // given scala.concurrent.ExecutionContext =
+  //   scala.concurrent.ExecutionContext.global
+
+  import ctx.{*, given}
+
+  def apply[F[_]: TransactionRepository]: TransactionRepository[F] =
+    summon
+
+  def getPage[F[_]: Async](
+      pageNavInfo: PageNavigation,
+  ): EitherT[F, String, PageResponse[Tx]] =
+    // OFFSET 시작번호, limit 페이지보여줄갯수
+    val cntQuery = quote {
+      query[Tx]
+    }
+
+    def pagedQuery =
+      quote { (pageNavInfo: PageNavigation) =>
+        val sizePerRequest = pageNavInfo.sizePerRequest
+        val offset         = sizePerRequest * pageNavInfo.pageNo
+        // val orderBy        = pageNavInfo.orderBy()
+
+        query[Tx]
+          .sortBy(t => t.eventTime)(Ord.desc)
+          .drop(offset)
+          .take(sizePerRequest)
+      }
+
+    val res = for
+      a <- countQuery(cntQuery)
+      b <- seqQuery(pagedQuery(lift(pageNavInfo)))
+    yield (a, b)
+
+    res.map { (totalCnt, r) =>
+      val totalPages = calTotalPage(totalCnt, pageNavInfo.sizePerRequest)
+      new PageResponse(totalCnt, totalPages, r)
+    }
+
+  def get[F[_]: Async](
+      hash: String,
+  )(using ExecutionContext): EitherT[F, String, Option[Tx]] =
+    inline def detailQuery =
+      quote { (hash: String) =>
+        query[Tx].filter(tx => tx.hash == hash).take(1)
+      }
+
+    optionQuery(detailQuery(lift(hash)))
+
+  def getPageByAccount[F[_]: Async](
+      pageNavInfo: PageNavigation,
+      addr: String,
+  )(using ExecutionContext): EitherT[F, String, PageResponse[Tx]] =
+    val cntQuery = quote {
+      query[Tx]
+    }
+
+    def pagedQuery =
+      quote { (pageNavInfo: PageNavigation) =>
+        val sizePerRequest = pageNavInfo.sizePerRequest
+        val offset         = sizePerRequest * pageNavInfo.pageNo
+        // val orderBy        = pageNavInfo.orderBy()
+
+        query[Tx]
+          .filter(t =>
+            t.fromAddr == lift(addr) || t.toAddr.contains(lift(addr)),
+          )
+          .sortBy(t => t.eventTime)(Ord.desc)
+          .drop(offset)
+          .take(sizePerRequest)
+      }
+
+    val res = for
+      a <- countQuery(cntQuery)
+      b <- seqQuery(pagedQuery(lift(pageNavInfo)))
+    yield (a, b)
+
+    res.map { (totalCnt, r) =>
+      val totalPages = calTotalPage(totalCnt, pageNavInfo.sizePerRequest)
+      new PageResponse(totalCnt, totalPages, r)
+    }
+
+/*
+    처음 10개의 게시글(ROW)를 가져온다.
+    SELECT * FROM BBS_TABLE LIMIT 10 OFFSET 0
+
+    11번째부터 10개의 게시글(ROW)를 가져온다.
+    SELECT * FROM BBS_TABLE LIMIT 10 OFFSET 10
+ */
