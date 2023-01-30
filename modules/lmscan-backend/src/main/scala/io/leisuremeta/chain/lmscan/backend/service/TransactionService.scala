201d3f2d9c55dfd504123660eda5bf9957fe67f3
jichangho <jichangho20@gmail.com>
2023-01-30 15:50:48 +0900

    add getPageByFilter to transactionService

--- modules/lmscan-backend/src/main/scala/io/leisuremeta/chain/lmscan/backend/service/TransactionService.scala
+++ modules/lmscan-backend/src/main/scala/io/leisuremeta/chain/lmscan/backend/service/TransactionService.scala
@@ -3,6 +3,11 @@ package io.leisuremeta.chain.lmscan.backend.service
 import io.leisuremeta.chain.lmscan.backend.entity.Tx
 import io.leisuremeta.chain.lmscan.backend.model.PageNavigation
 import io.leisuremeta.chain.lmscan.backend.model.PageResponse
+import io.leisuremeta.chain.lmscan.backend.model.{
+  TxDetail,
+  TransferHist,
+  TxInfo,
+}
 import io.leisuremeta.chain.lmscan.backend.repository.TransactionRepository
 import cats.Functor
 import cats.data.EitherT
@@ -16,33 +21,62 @@ object TransactionService:
 
   def get[F[_]: Async](
       hash: String,
-  ): EitherT[F, String, Option[Tx]] =
-    TransactionRepository.get(hash)
+  ): EitherT[F, String, Option[TxDetail]] =
+    for
+      trx <- TransactionRepository.get(hash)
+      detail = trx.map { tx =>
+        TxDetail(
+          tx.hash,
+          tx.createdAt,
+          tx.fromAddr,
+          tx.txType,
+          tx.tokenType,
+          tx.inputHashs,
+          tx.outputVals.map { (ftv: String) =>
+            val items = ftv.split("/")
+            TransferHist(
+              items(0),
+              items(1),
+            )
+          },
+          tx.json,
+        )
+      }
+    yield detail
 
   // EitherT[F[_], A, B] is a lightweight wrapper for F[Either[A, B]]
   // that makes it easy to compose Eithers and Fs together.
   def getPage[F[_]: Async](
       pageNavInfo: PageNavigation,
-  ): EitherT[F, String, PageResponse[Tx]] =
-    TransactionRepository.getPage(pageNavInfo)
+  ): EitherT[F, String, PageResponse[TxInfo]] =
+    for
+      page <- TransactionRepository.getPage(pageNavInfo)
+      txInfo = convertToInfo(page.payload)
+    yield PageResponse(page.totalCount, page.totalPages, txInfo)
 
   def getPageByAccount[F[_]: Async](
       address: String,
       pageNavInfo: PageNavigation,
-  ): EitherT[F, String, PageResponse[Tx]] =
-    TransactionRepository.getPageByAccount(address, pageNavInfo)
+  ): EitherT[F, String, PageResponse[TxInfo]] =
+    for
+      page <- TransactionRepository.getPageByAccount(address, pageNavInfo)
+      txInfo = convertToInfo(page.payload)
+    yield PageResponse(page.totalCount, page.totalPages, txInfo)
 
   def getPageByBlock[F[_]: Async](
       blockHash: String,
       pageNavInfo: PageNavigation,
-  ): EitherT[F, String, PageResponse[Tx]] =
-    TransactionRepository.getTxPageByBlock(blockHash, pageNavInfo)
+  ): EitherT[F, String, PageResponse[TxInfo]] =
+    for
+      page <- TransactionRepository.getTxPageByBlock(blockHash, pageNavInfo)
+      txInfo = convertToInfo(page.payload)
+    yield PageResponse(page.totalCount, page.totalPages, txInfo)
 
   def getPageByFilter[F[_]: Async](
       pageInfo: PageNavigation,
       blockHash: Option[String],
       accountAddr: Option[String],
-  ): EitherT[F, String, PageResponse[Tx]] =
+  ): EitherT[F, String, PageResponse[TxInfo]] =
     (accountAddr, blockHash) match
       case (None, None) => getPage[F](pageInfo)
       case (None, Some(blockHash)) =>
@@ -51,6 +85,19 @@ object TransactionService:
         getPageByAccount[F](accountAddr, pageInfo)
       case (_, _) =>
         throw new RuntimeException("검색 파라미터를 하나만 입력해주세요.")
-      // (ExploreApi
-      //   .ServerError("검색 파라미터를 하나만 입력해주세요."))
-      //   .asLeft[ExploreApi.UserError],
+
+  def convertToInfo(txs: Seq[Tx]): Seq[TxInfo] =
+    txs.map { tx =>
+      val latestOutVal = tx.outputVals.headOption match
+        case Some(str) => str.split("/")
+        case None      => Array[String]("", "")
+      TxInfo(
+        tx.hash,
+        tx.blockNumber,
+        tx.eventTime,
+        tx.txType,
+        tx.tokenType,
+        tx.fromAddr,
+        latestOutVal(1),
+      )
+    }
