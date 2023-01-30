f99995b60605a5cd7a282b2b93d6573fd2fb3dfe
jichangho <jichangho20@gmail.com>
2023-01-30 15:47:21 +0900

    change txHistory field type

--- modules/lmscan-backend/src/main/scala/io/leisuremeta/chain/lmscan/backend/model/AccountDetail.scala
+++ modules/lmscan-backend/src/main/scala/io/leisuremeta/chain/lmscan/backend/model/AccountDetail.scala
@@ -1,12 +1,13 @@
 package io.leisuremeta.chain.lmscan.backend.model
 
-import io.leisuremeta.chain.lmscan.backend.entity.{Tx, Account}
+import io.leisuremeta.chain.lmscan.backend.entity.Account
+import io.leisuremeta.chain.lmscan.backend.model.TxInfo
 
 final case class AccountDetail(
     address: String,
     balance: Double,
     value: Double,
-    txHistory: Seq[Tx],
+    txHistory: Seq[TxInfo],
 ):
-  def this(acc: Account, txHist: Seq[Tx]) =
+  def this(acc: Account, txHist: Seq[TxInfo]) =
     this(acc.address, acc.balance, acc.amount, txHist)
