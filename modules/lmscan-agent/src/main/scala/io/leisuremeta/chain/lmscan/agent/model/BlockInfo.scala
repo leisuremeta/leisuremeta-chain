2b8181dc61121a70c30f076a988d5d80a951c4d0
jichangho <jichangho20@gmail.com>
2023-01-30 09:39:27 +0900

    add temp

---
+++ modules/lmscan-agent/src/main/scala/io/leisuremeta/chain/lmscan/agent/model/BlockInfo.scala
@@ -0,0 +1,9 @@
+package io.leisuremeta.chain.lmscan.agent.model
+
+import java.time.Instant
+
+final case class BlockInfo(
+    blockNumber: BigInt,
+    timestamp: Instant,
+    blockHash: String,
+)
