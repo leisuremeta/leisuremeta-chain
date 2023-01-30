3af64ce4965261a964187c707b389a59e2cc0856
jichangho <jichangho20@gmail.com>
2023-01-30 15:46:18 +0900

    refactor Tx entity class

--- modules/lmscan-backend/src/main/scala/io/leisuremeta/chain/lmscan/backend/entity/Tx.scala
+++ modules/lmscan-backend/src/main/scala/io/leisuremeta/chain/lmscan/backend/entity/Tx.scala
@@ -6,10 +6,15 @@ import io.getquill.Quoted
 final case class Tx(
     hash: String,
     txType: String, // col_name : type
+    tokenType: String,
     fromAddr: String,
     toAddr: Seq[String],
     amount: Double, // value
     blockHash: String,
+    blockNumber: Long,
     eventTime: Long,
     createdAt: Long,
+    inputHashs: Seq[String],
+    outputVals: Seq[String],
+    json: String,
 )
