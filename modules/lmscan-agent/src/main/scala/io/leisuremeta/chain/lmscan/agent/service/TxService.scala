14785f25e17b30dff76a4e2f3771312ae47d5549
jichangho20@gmail.com <>
2023-01-31 22:22:23 +0900

    add transaction service insert method

--- modules/lmscan-agent/src/main/scala/io/leisuremeta/chain/lmscan/agent/service/TxService.scala
+++ modules/lmscan-agent/src/main/scala/io/leisuremeta/chain/lmscan/agent/service/TxService.scala
@@ -1,3 +1,12 @@
 package io.leisuremeta.chain.lmscan.agent.service
 
+import cats.effect.kernel.Async
+import cats.data.EitherT
+import io.leisuremeta.chain.lmscan.agent.entity.Tx
+import io.leisuremeta.chain.lmscan.agent.repository.TxRepository
+
 object TxService:
+  def insert[F[_]: Async](
+    tx: Tx
+  ): EitherT[F, String, Long] =
+    TxRepository.insert(tx)
