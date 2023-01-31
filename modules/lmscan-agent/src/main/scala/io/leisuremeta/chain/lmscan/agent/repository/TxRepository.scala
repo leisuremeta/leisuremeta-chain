f49d4acd8b156c9440e83e6e4a62cffb539df780
jichangho20@gmail.com <>
2023-01-31 22:21:39 +0900

    add txRepository

--- modules/lmscan-agent/src/main/scala/io/leisuremeta/chain/lmscan/agent/repository/TxRepository.scala
+++ modules/lmscan-agent/src/main/scala/io/leisuremeta/chain/lmscan/agent/repository/TxRepository.scala
@@ -6,6 +6,7 @@ import cats.data.EitherT
 import io.getquill.*
 import cats.effect.IO
 import io.getquill.Query
+import io.getquill.context.*
 import io.leisuremeta.chain.lmscan.agent.repository.CommonQuery
 
 case class Person(name: String, age: Int)
@@ -13,41 +14,11 @@ case class Person(name: String, age: Int)
 object TxRepository extends CommonQuery:
   import ctx.{*, given}
 
-  def insert[F[_]: Async](tx: Tx): EitherT[F, String, Boolean] =
-    // inline def upsertQuery =
-    //   quote { (tx: Tx) =>
-    //     query[Tx]
-    //       .insert(_.hash ->)
-    //   }
-    // inline def a = quote {
-    //   liftQuery(List(Person("John", 31), Person("name2", 32)))
-    //     .foreach(e => query[Person].insert(_.name -> e.name, _.age -> e.age))
-    //     .returning(_.id)
-    // }
-    ???
+  def insert[F[_]: Async](tx: Tx): EitherT[F, String, Long] =
+    inline def upsertQuery =
+      quote { (tx: Tx) =>
+        query[Tx].insertValue(tx)
+      }
+    super.insert(upsertQuery(lift(tx)))
+    
 
-  def insertBatch[F[_]: Async](txs: List[Tx]): EitherT[F, String, String] =
-    val batchInsert = quote {
-      liftQuery(lift(txs))
-        .foreach(tx =>
-          query[Tx]
-            .insertValue(tx)
-            .returning(x => x),
-        )
-    }
-
-    // liftQuery(vips).foreach(v =>
-    //   query[Person].insertValue(Person(v.first + v.last, v.age)).returning(_.id),
-    // )
-
-    super.insert(batchInsert)
-
-// case class Product(id: Int, description: String, sku: Long)
-
-/*
-val a = quote {
-  liftQuery(List(Person(0, "John", 31),Person(0, "name2", 32)))
-    .foreach(e => query[Person].insert(_.name -> p.name, _.age -> p.age))
-    .returning(_.id)
-}
- */
