19e2c6b1c463519c04a6762ad4d2eda2fab91f94
jichangho <jichangho20@gmail.com>
2023-01-31 11:29:23 +0900

    add blockInfo model class

--- modules/lmscan-agent/src/main/scala/io/leisuremeta/chain/lmscan/agent/model/BlockInfo.scala
+++ modules/lmscan-agent/src/main/scala/io/leisuremeta/chain/lmscan/agent/model/BlockInfo.scala
@@ -3,7 +3,8 @@ package io.leisuremeta.chain.lmscan.agent.model
 import java.time.Instant
 
 final case class BlockInfo(
-    blockNumber: BigInt,
-    timestamp: Instant,
-    blockHash: String,
+  blockNumber: BigInt,
+  timestamp: Instant,
+  blockHash: PBlock,
+  txCount: Int,
 )
