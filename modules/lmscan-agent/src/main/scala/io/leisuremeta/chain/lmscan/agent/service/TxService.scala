033c718ac90dc0082ee16e466c5e80de009a1cc5
jichangho <jichangho20@gmail.com>
2023-02-01 16:08:54 +0900

    temp

--- modules/lmscan-agent/src/main/scala/io/leisuremeta/chain/lmscan/agent/service/TxService.scala
+++ modules/lmscan-agent/src/main/scala/io/leisuremeta/chain/lmscan/agent/service/TxService.scala
@@ -1,12 +1,12 @@
-package io.leisuremeta.chain.lmscan.agent.service
+// package io.leisuremeta.chain.lmscan.agent.service
 
-import cats.effect.kernel.Async
-import cats.data.EitherT
-import io.leisuremeta.chain.lmscan.agent.entity.Tx
-import io.leisuremeta.chain.lmscan.agent.repository.TxRepository
+// import io.leisuremeta.chain.lmscan.agent.entity.Tx
+// import cats.effect.kernel.Async
+// import cats.data.EitherT
+// import io.leisuremeta.chain.lmscan.agent.repository.TxRepository
 
-object TxService:
-  def insert[F[_]: Async](
-    tx: Tx
-  ): EitherT[F, String, Long] =
-    TxRepository.insert(tx)
+// object TxService:
+//   def insert[F[_]: Async](
+//       tx: Tx,
+//   ): EitherT[F, String, Long] =
+//     TxRepository.insertWithoutTransaction(tx)
