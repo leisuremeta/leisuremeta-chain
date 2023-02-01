5822e773c29ebf0cdc56f4eefb7959211bee918d
jichangho <jichangho20@gmail.com>
2023-02-01 16:07:04 +0900

    add properties

--- modules/lmscan-agent/src/main/scala/io/leisuremeta/chain/lmscan/agent/entity/Tx.scala
+++ modules/lmscan-agent/src/main/scala/io/leisuremeta/chain/lmscan/agent/entity/Tx.scala
@@ -3,10 +3,14 @@ package io.leisuremeta.chain.lmscan.agent.entity
 final case class Tx(
     hash: String,
     txType: String, // col_name : type
+    tokenType: String,
     fromAddr: String,
     toAddr: Seq[String],
-    amount: Double, // value
     blockHash: String,
+    blockNumber: Long,
     eventTime: Long,
     createdAt: Long,
+    inputHashs: Seq[String],
+    outputVals: Seq[String],
+    json: String,
 )
