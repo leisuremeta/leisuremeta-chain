e8cc2ecd1d4e8cf860b7d48b45224fd8217ebffc
jichangho <jichangho20@gmail.com>
2023-02-01 16:07:40 +0900

    add db transaction logic in protoquill

--- modules/lmscan-agent/src/main/scala/io/leisuremeta/chain/lmscan/agent/repository/CommonQuery.scala
+++ modules/lmscan-agent/src/main/scala/io/leisuremeta/chain/lmscan/agent/repository/CommonQuery.scala
@@ -12,9 +12,14 @@ import io.getquill.*
 import io.getquill.Literal
 import scala.concurrent.ExecutionContext.global
 import scala.concurrent.ExecutionContext
+import scala.concurrent.ExecutionContext.Implicits.{global as ec}
+import io.leisuremeta.chain.lmscan.agent.entity.Tx
+import io.getquill.Query
+import java.time.Instant
 
 trait CommonQuery:
   val ctx = new PostgresJAsyncContext(SnakeCase, "ctx")
+  import ctx.{*, given}
 
   inline def optionQuery[F[_]: Async, T](
       inline query: Query[T],
@@ -35,17 +40,104 @@ trait CommonQuery:
       }
     }
 
-  inline def insert[F[_]: Async, T](
-      inline query: Insert[T],
+  inline def insertWithoutTransaction[F[_]: Async, T](
+      tx: Tx,
   ): EitherT[F, String, Long] =
+    println("222222")
     EitherT {
       Async[F].recover {
         for
           given ExecutionContext <- Async[F].executionContext
           ids <- Async[F]
-            .fromFuture(Async[F].delay {
-              ctx.run(query)
-              // InternalApi.runBatchActionReturning(query)
+            .fromCompletableFuture(Async[F].delay {
+              println("333333")
+              ctx.transaction[Long] {
+                for p <- ctx
+                    .run(
+                      quote {
+                        query[Tx]
+                          .insertValue(
+                            lift(tx),
+                          )
+                          .onConflictUpdate(_.hash)((t, e) => t.hash -> e.hash)
+                      },
+                    )
+                yield p
+              }
+            })
+            .map(Either.right(_))
+        yield
+          scribe.info("444444")
+          ids
+      } {
+        case e: SQLException =>
+          scribe.info("55555")
+          Left(s"sql exception occured: " + e.getMessage())
+        case e: Exception =>
+          scribe.info("66666: " + e.getMessage())
+          Left(e.getMessage())
+      }
+    }
+
+  // inline def insert[F[_]: Async, T](): EitherT[F, String, Long] =
+  //   println("222222")
+  //   EitherT {
+  //     Async[F].recover {
+  //       for
+  //         given ExecutionContext <- Async[F].executionContext
+  //         ids <- Async[F]
+  //           .fromCompletableFuture(Async[F].delay {
+  //             println("333333")
+  //             ctx.transaction { implicit ec =>
+  //               for p <- ctx
+  //                   .run(
+  //                     quote {
+  //                       query[Tx].insertValue(
+  //                         lift(
+  //                           Tx(
+  //                             "123",
+  //                             "account",
+  //                             "1234",
+  //                             "123a",
+  //                             Instant.now().toEpochMilli(),
+  //                           ),
+  //                         ),
+  //                       )
+  //                     },
+  //                   )
+  //               yield p
+  //             // ctx.run(query)
+  //             }
+  //           })
+  //           .map(Either.right(_))
+  //       yield
+  //         scribe.info("444444")
+  //         ids
+  //     } {
+  //       case e: SQLException =>
+  //         scribe.info("55555")
+  //         Left(s"sql exception occured: " + e.getMessage())
+  //       case e: Exception =>
+  //         scribe.info("66666: " + e.getMessage())
+  //         Left(e.getMessage())
+  //     }
+  //   }
+
+  inline def insertBatch[F[_]: Async, T](
+      // inline query: Insert[T],
+      inline query: BatchAction[Insert[T]],
+  ): EitherT[F, String, Seq[Long]] =
+    EitherT {
+      Async[F].recover {
+        for
+          // given ExecutionContext <- Async[F].executionContext
+          ids <- Async[F]
+            .fromCompletableFuture(Async[F].delay {
+              ctx.transaction { implicit ec =>
+                for p <- ctx.run(query)
+                yield p
+              // ctx.run(query)
+              }
             })
             .map(Either.right(_))
         yield ids
