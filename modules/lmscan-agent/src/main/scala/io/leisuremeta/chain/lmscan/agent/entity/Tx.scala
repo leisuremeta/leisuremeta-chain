626e41d9073a9efddb349f5c535a3463ccb6a8a9
jichangho <jichangho20@gmail.com>
2023-02-02 09:03:42 +0900

    add tx entity extends id trait

--- modules/lmscan-agent/src/main/scala/io/leisuremeta/chain/lmscan/agent/entity/Tx.scala
+++ modules/lmscan-agent/src/main/scala/io/leisuremeta/chain/lmscan/agent/entity/Tx.scala
@@ -1,4 +1,5 @@
 package io.leisuremeta.chain.lmscan.agent.entity
+import io.leisuremeta.chain.lmscan.agent.model.id
 
 final case class Tx(
     hash: String,
@@ -13,4 +14,6 @@ final case class Tx(
     inputHashs: Seq[String],
     outputVals: Seq[String],
     json: String,
-)
+) extends id:
+    def id: String =
+        this.hash
