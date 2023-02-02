a527e3e35639ba7e3636c5739cd08ef25797e734
jichangho <jichangho20@gmail.com>
2023-02-02 09:01:50 +0900

    add account entity extends id trait

--- modules/lmscan-agent/src/main/scala/io/leisuremeta/chain/lmscan/agent/entity/Account.scala
+++ modules/lmscan-agent/src/main/scala/io/leisuremeta/chain/lmscan/agent/entity/Account.scala
@@ -1,8 +1,12 @@
 package io.leisuremeta.chain.lmscan.agent.entity
 
+import io.leisuremeta.chain.lmscan.agent.model.id
+
 final case class Account(
     address: String,
     balance: Double,
     amount: Double,
     createdAt: Long,
-)
+) extends id:
+    def id: String =
+        this.address
