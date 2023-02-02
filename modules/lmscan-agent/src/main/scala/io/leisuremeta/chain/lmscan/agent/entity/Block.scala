7acc2ae768d4b1e1c1898619eedfd1c3dd41e3a8
jichangho <jichangho20@gmail.com>
2023-02-02 13:45:49 +0900

    remove id tratit

--- modules/lmscan-agent/src/main/scala/io/leisuremeta/chain/lmscan/agent/entity/Block.scala
+++ modules/lmscan-agent/src/main/scala/io/leisuremeta/chain/lmscan/agent/entity/Block.scala
@@ -1,14 +1,10 @@
 package io.leisuremeta.chain.lmscan.agent.entity
 
-import io.leisuremeta.chain.lmscan.agent.model.id
-
 final case class Block(
-    number: Long,
     hash: String,
+    number: Long,
     parentHash: String,
     txCount: Long,
     eventTime: Long,
     createdAt: Long,
-) extends id:
-    def id: String =
-        this.hash
+) 
