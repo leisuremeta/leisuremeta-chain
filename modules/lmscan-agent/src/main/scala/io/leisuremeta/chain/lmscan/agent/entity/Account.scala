2bce3781ebd68ee6a74038bedef6c31b10e38f2e
jichangho <jichangho20@gmail.com>
2023-02-02 13:45:22 +0900

    remove id tratit

--- modules/lmscan-agent/src/main/scala/io/leisuremeta/chain/lmscan/agent/entity/Account.scala
+++ modules/lmscan-agent/src/main/scala/io/leisuremeta/chain/lmscan/agent/entity/Account.scala
@@ -1,12 +1,9 @@
 package io.leisuremeta.chain.lmscan.agent.entity
 
-import io.leisuremeta.chain.lmscan.agent.model.id
 
 final case class Account(
     address: String,
     balance: Double,
     amount: Double,
     createdAt: Long,
-) extends id:
-    def id: String =
-        this.address
+) 
