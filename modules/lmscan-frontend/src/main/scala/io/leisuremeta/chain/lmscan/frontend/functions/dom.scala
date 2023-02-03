6bb77546297b4b64e8f0fbfc349f219a035ec04c
G-yhlee <yhleesoft@gmail.com>
2023-02-03 14:51:52 +0900

    add plural feature

--- modules/lmscan-frontend/src/main/scala/io/leisuremeta/chain/lmscan/frontend/functions/dom.scala
+++ modules/lmscan-frontend/src/main/scala/io/leisuremeta/chain/lmscan/frontend/functions/dom.scala
@@ -1,6 +1,7 @@
 package io.leisuremeta.chain.lmscan.frontend
 import org.scalajs.dom.window
 import scala.scalajs.js.{Date}
+import scala.concurrent.duration.*
 
 object Dom:
   // currently only use className selection, but could be update as more generic fuction
@@ -16,6 +17,32 @@ object Dom:
       .toISOString()
       .slice(0, 19)
       .replace("T", " ")
+    timeAgo(timestamp)
 
   // [timestamp] => YYYY-MM-DD TIME
   //  1673939878 =>  1970-01-20 08:58:59
+
+  def timeAgo(timestamp: Double) =
+    val now     = Date.now()
+    val from    = timestamp
+    val timeGap = (now - from) / 1000
+
+    val times = List(
+      ((timeGap / 31536000).toInt.toString(), "year ago"),
+      ((timeGap / 2592000).toInt.toString(), "month ago"),
+      ((timeGap / 86400).toInt.toString(), "day ago"),
+      ((timeGap / 3600).toInt.toString(), "hour ago"),
+      ((timeGap / 60).toInt.toString(), "min ago"),
+      ((timeGap / 1).toInt.toString(), "s ago"),
+    )
+
+    times
+      .filter((time, msg) => !time.startsWith("0"))
+      .map((time, msg) =>
+        time + {
+          // fix plural
+          time.toInt > 1 match
+            case true  => msg.replace(" ", "s ").replace("ss", "s")
+            case false => msg
+        },
+      )(0)
