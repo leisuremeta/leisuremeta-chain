5714cfffdc6e86eacc1059150a03eca55be9033a
jichangho <jichangho20@gmail.com>
2023-02-03 15:54:08 +0900

    rename account to account entity file for avoiding duplication with account model file of api module

---
+++ modules/lmscan-agent/src/main/scala/io/leisuremeta/chain/lmscan/agent/entity/AccountEntity.scala
@@ -0,0 +1,21 @@
+package io.leisuremeta.chain.lmscan.agent.entity
+
+import io.leisuremeta.chain.api.model.Transaction.AccountTx
+import java.time.Instant
+
+
+final case class AccountEntity(
+    address: String,
+    balance: Double,
+    amount: Double,
+    eventTime: Long,
+    createdAt: Long,
+):
+  def from(tx: AccountTx) =
+    AccountEntity(
+      tx.account.utf8.value,
+      0,
+      0,
+      tx.createdAt.getEpochSecond(),
+      Instant.now().getEpochSecond(),
+    )
