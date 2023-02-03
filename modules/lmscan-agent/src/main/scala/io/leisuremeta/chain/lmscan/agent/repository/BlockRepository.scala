e522dc04d399117744f2be01d2bf173bf2f7a785
jichangho <jichangho20@gmail.com>
2023-02-03 09:42:18 +0900

    add block repo

--- modules/lmscan-agent/src/main/scala/io/leisuremeta/chain/lmscan/agent/repository/BlockRepository.scala
+++ modules/lmscan-agent/src/main/scala/io/leisuremeta/chain/lmscan/agent/repository/BlockRepository.scala
@@ -2,7 +2,7 @@ package io.leisuremeta.chain.lmscan.agent.repository
 
 import cats.effect.kernel.Async
 import cats.data.EitherT
-import io.leisuremeta.chain.lmscan.agent.entity.Block
+import io.leisuremeta.chain.lmscan.agent.entity.{Block, BlockSavedLog}
 import io.getquill.*
 
 object BlockRepository extends CommonQuery:
@@ -18,9 +18,17 @@ object BlockRepository extends CommonQuery:
       }
     optionQuery(detailQuery(lift(hash)))
 
-  def insert[F[_]: Async](block: Block): EitherT[F, String, Long] =
-    println("11111")
-    insertTransaction(block)
+  // def insert[F[_]: Async](block: Block): EitherT[F, String, Long] =
+  //   println("11111")
+  //   insertTransaction(block)
+
+  def getLastSavedBlock[F[_]: Async]: EitherT[F, String, Option[BlockSavedLog]] =
+    inline def latestQuery =
+      quote { 
+        query[BlockSavedLog].sortBy(t => t.eventTime)(Ord.desc).take(1)
+      }
+    optionQuery(latestQuery)
+  
   /*
 val a = quote {
   liftQuery(List(Person(0, "John", 31),Person(0, "name2", 32)))
