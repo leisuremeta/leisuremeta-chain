12aacb90d3e1ee6181395ea1b2c2040305d42c54
jichangho <jichangho20@gmail.com>
2023-02-06 11:03:07 +0900

    temp

--- modules/lmscan-agent/src/main/scala/io/leisuremeta/chain/lmscan/agent/entity/BlockSavedLog.scala
+++ modules/lmscan-agent/src/main/scala/io/leisuremeta/chain/lmscan/agent/entity/BlockSavedLog.scala
@@ -3,6 +3,7 @@ package io.leisuremeta.chain.lmscan.agent.entity
 final case class BlockSavedLog(
     hash: String,
     number: Long,
+    json: String,
     eventTime: Long,
     createdAt: Long,
 )
