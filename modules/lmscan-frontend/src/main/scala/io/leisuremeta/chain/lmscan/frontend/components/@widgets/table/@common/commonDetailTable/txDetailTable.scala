1ff479e8fe86c2a5aacdb54c4fe521efe755ea51
G-yhlee <yhleesoft@gmail.com>
2023-02-08 16:51:48 +0900

    temp refactoring

--- modules/lmscan-frontend/src/main/scala/io/leisuremeta/chain/lmscan/frontend/components/@widgets/table/@common/commonDetailTable/txDetailTable.scala
+++ modules/lmscan-frontend/src/main/scala/io/leisuremeta/chain/lmscan/frontend/components/@widgets/table/@common/commonDetailTable/txDetailTable.scala
@@ -14,11 +14,11 @@ object TxDetailTable:
 
   // TODO :: zip 말고 더 좋은 방법?
   val input = (data: List[String], isModal: Boolean) =>
-    isModal match {
-      case true => data.zipWithIndex.map { ((input, i) => genInputForModal(input, i + 1)) }  
-      case false => data.zipWithIndex.map { ((input, i) => genInput(input, i + 1)) }  
-    }
-      
+    isModal match
+      case true =>
+        data.zipWithIndex.map { ((input, i) => genInputForModal(input, i + 1)) }
+      case false =>
+        data.zipWithIndex.map { ((input, i) => genInput(input, i + 1)) }
 
   val output = (data: List[Transfer]) =>
     data.zipWithIndex.map { case ((output), i) => genOutput(output, i + 1) }
@@ -39,14 +39,17 @@ object TxDetailTable:
     div(`class` := "row")(
       div(`class` := "cell type-detail-body")(i.toString()),
       div(`class` := "cell type-detail-body")(data),
-    )    
+    )
 
   val genOutput = (data: Transfer, i: Any) =>
     div(`class` := "row")(
       div(`class` := "cell type-detail-head")(i.toString()),
       div(`class` := "cell type-3 type-detail-body")(
         span(
-          onClick(NavMsg.AccountDetail(data.toAddress)),
+          // onClick(NavMsg.AccountDetail(data.toAddress)), // TODO :: 실데이터 받을때 이걸로 변경
+          onClick(
+            NavMsg.AccountDetail("26A463A0ED56A4A97D673A47C254728409C7B002"),
+          ),
         )(data.toAddress),
       ),
       div(`class` := "cell type-detail-body")(
@@ -58,7 +61,10 @@ object TxDetailTable:
       div(`class` := "cell type-detail-head")(i.toString()),
       div(`class` := "cell type-3 type-detail-body")(
         span(
-          onClick(NavMsg.AccountDetail(data.toAddress)),
+          // onClick(NavMsg.AccountDetail(data.toAddress)),
+          onClick(
+            NavMsg.AccountDetail("26A463A0ED56A4A97D673A47C254728409C7B002"),
+          ),
         )(data.toAddress),
       ),
       div(`class` := "cell type-3 type-detail-body")(
@@ -112,17 +118,18 @@ object TxDetailTable:
           div(`class` := "table w-[100%]")(
             div(`class` := "row")(
               div(`class` := "cell type-detail-head")("Input"),
-              div(`class` := "cell type-detail-body font-bold")("Transaction Hash"),
-              data.inputHashs.length > 5 match {
-                case true => 
+              div(`class` := "cell type-detail-body font-bold")(
+                "Transaction Hash",
+              ),
+              data.inputHashs.length > 5 match
+                case true =>
                   div(`class` := s"type-2 pt-16px")(
                     span(
                       `class` := s"${State.toggleTxDetailInput(model, ToggleMsg.ClickTxDetailInput, "_button")} ",
                       onClick(ToggleMsg.ClickTxDetailInput),
                     )("More"),
-                  )  
-                case false => div()              
-              }
+                  )
+                case false => div(),
             )
               :: input(data.inputHashs.slice(0, 5), false),
           ),
@@ -165,22 +172,22 @@ object TxDetailTable:
           `class` := s"${State.toggle(model, ToggleMsg.Click, "_textarea")}",
         )(s"${TxDetailParser.txDetailEncoder(data)}"),
       ),
-
       div(
-        `class` := s"${State.toggleTxDetailInput(model, ToggleMsg.ClickTxDetailInput, "_table")}",        
+        `class` := s"${State.toggleTxDetailInput(model, ToggleMsg.ClickTxDetailInput, "_table")}",
       )(
         div(`class` := "type-TableDetail table-container txDetailModalTable")(
           div(`class` := "table w-[100%]")(
             div(`class` := "row")(
               div(`class` := "cell type-detail-head")("Input"),
-              div(`class` := "cell type-detail-body font-bold")("Transaction Hash"),
+              div(`class` := "cell type-detail-body font-bold")(
+                "Transaction Hash",
+              ),
               div(`class` := s"type-2 pt-16px")(
-                span(onClick(ToggleMsg.ClickTxDetailInput))("Close")
+                span(onClick(ToggleMsg.ClickTxDetailInput))("Close"),
               ),
             )
               :: input(data.inputHashs, true),
           ),
         ),
-      ),                
-
+      ),
     )
