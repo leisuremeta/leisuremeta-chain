d80e1b5c62359dd410d2653cfd073f96470f6c5e
BK <121482953+BKwork2@users.noreply.github.com>
2023-02-09 11:10:49 +0900

    모든 view 빈값 예외처리 적용

--- modules/lmscan-frontend/src/main/scala/io/leisuremeta/chain/lmscan/frontend/functions/getFieldValue.scala
+++
@@ -1,6 +0,0 @@
-package io.leisuremeta.chain.lmscan.frontend
-
-object CommonFunc:
-  def getFieldValue = (field: Option[Any]) => field match 
-    case Some(value) => value.toString()
-    case None => "-"
