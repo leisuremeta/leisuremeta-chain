496f9d573a141a8f0250ed8914d388a579c65865
G-yhlee <yhleesoft@gmail.com>
2023-02-13 10:53:46 +0900

    scalajs linker.runtime.undefinedBehaviorError:jaba.lang.ClassCastException ...에러 fix

--- modules/lmscan-frontend/assets/load-main.js
+++ modules/lmscan-frontend/assets/load-main.js
@@ -1,9 +1,9 @@
-if (process.env.NODE_ENV === 'production') {
-  import('../target/scala-3.2.1/leisuremeta-chain-lmscan-frontend-opt/main.js').then(
-    (LmScan) => LmScan.launchApp(process.env.BACKEND_URL, process.env.BACKEND_PORT)
-  );
+if (process.env.NODE_ENV === "production") {
+  import(
+    "../target/scala-3.2.1/leisuremeta-chain-lmscan-frontend-opt/main.js"
+  ).then((LmScan) => LmScan.launchApp(process.env.BASE_API_URL));
 } else {
-  import('../target/scala-3.2.1/leisuremeta-chain-lmscan-frontend-fastopt/main.js').then(
-    (LmScan) => LmScan.launchApp(process.env.BACKEND_URL, process.env.BACKEND_PORT)
-  );
+  import(
+    "../target/scala-3.2.1/leisuremeta-chain-lmscan-frontend-fastopt/main.js"
+  ).then((LmScan) => LmScan.launchApp(process.env.BASE_API_URL));
 }
