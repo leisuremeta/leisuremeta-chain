e6b53585db8c9ae279dcbd38b2b908337121cfcc
jichangho <jichangho20@gmail.com>
2023-02-16 15:26:29 +0900

    temp

--- modules/lmscan-agent/src/main/scala/io/leisuremeta/chain/lmscan/agent/service/BlockService.scala
+++ modules/lmscan-agent/src/main/scala/io/leisuremeta/chain/lmscan/agent/service/BlockService.scala
@@ -3,8 +3,7 @@ package io.leisuremeta.chain.lmscan.agent.service
 import cats.data.EitherT
 import cats.effect.kernel.Async
 import io.leisuremeta.chain.lmscan.agent.repository.BlockRepository
-import io.leisuremeta.chain.lmscan.agent.entity.{BlockSavedLog}
-import io.leisuremeta.chain.lmscan.agent.entity.BlockEntity
+import io.leisuremeta.chain.lmscan.agent.entity.{BlockSavedLog, BlockStateEntity, BlockEntity}
 
 object BlockService:
   def get[F[_]: Async](
@@ -21,6 +20,10 @@ object BlockService:
   def getLastSavedBlock[F[_]: Async]: 
     EitherT[F, String, Option[BlockSavedLog]] =
     BlockRepository.getLastSavedBlock
+
+  def getLastBuildedBlock[F[_]: Async]:
+    EitherT[F, String, Option[BlockStateEntity]]
+    BlockRepository.getLastBuildedBlock()
 
   // def countBlockNumber[F[_]: Async]: EitherT[F, String, Option[Long]]
   //   BlockRepository.getL
