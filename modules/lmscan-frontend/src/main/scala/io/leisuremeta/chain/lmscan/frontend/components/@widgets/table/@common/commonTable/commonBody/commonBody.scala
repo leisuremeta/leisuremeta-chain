7a148c65505bdb44e18f9d326cb98995524756e4
G-yhlee <yhleesoft@gmail.com>
2023-02-19 00:42:17 +0900

    refactor nftTable

--- modules/lmscan-frontend/src/main/scala/io/leisuremeta/chain/lmscan/frontend/components/@widgets/table/@common/commonTable/commonBody/commonBody.scala
+++ modules/lmscan-frontend/src/main/scala/io/leisuremeta/chain/lmscan/frontend/components/@widgets/table/@common/commonTable/commonBody/commonBody.scala
@@ -1,122 +1,35 @@
 package io.leisuremeta.chain.lmscan.frontend
 
 import tyrian.Html.*
-// package io.leisuremeta.chain.lmscan.frontend
-// import Dom.{_hidden, isEqGet}
 import ValidOutputData.*
 import Dom.*
 
 object Body:
   def block = (payload: List[Block]) =>
     payload
-      .map(each =>
+      .map(v =>
         div(`class` := "row table-body")(
           gen.cell(
-            Cell.BLOCK_NUMBER(each.hash, each.number),
-            Cell.AGE(each.createdAt),
-            Cell.BLOCK_HASH(each.hash),
-            Cell.PlainInt(each.txCount),
+            Cell.BLOCK_NUMBER(v.hash, v.number),
+            Cell.AGE(v.createdAt),
+            Cell.BLOCK_HASH(v.hash),
+            Cell.PlainInt(v.txCount),
           ),
         ),
       )
 
   def nft = (payload: List[NftActivities]) =>
     payload
-      .map(each =>
-        val from = getOptionValue(each.fromAddr, "-").toString()
-        val to   = getOptionValue(each.toAddr, "-").toString()
-
+      .map(v =>
         div(
           `class` := "row table-body",
         )(
-          div(`class` := "cell type-3")(
-            span(
-              onClick(
-                PageMsg.PreUpdate(
-                  PageName.TransactionDetail(
-                    getOptionValue(each.txHash, "-").toString(),
-                  ),
-                ),
-              ),
-            )(
-              getOptionValue(each.txHash, "-")
-                .toString(),
-                // .take(10) + "...",
-            ),
-          ),
-          div(`class` := "cell")(
-            span()(
-              yyyy_mm_dd_time(
-                getOptionValue(each.createdAt, 0).asInstanceOf[Int],
-              ),
-            ),
-          ),
-          div(`class` := "cell")(
-            span()(getOptionValue(each.action, "-").toString()),
-          ),
-          div(`class` := "cell type-3")(
-            span(
-              onClick(
-                PageMsg.PreUpdate(
-                  PageName.AccountDetail(
-                    getOptionValue(
-                      each.fromAddr,
-                      "-",
-                    )
-                      .toString(),
-                  ),
-                ),
-              ),
-            )(
-              from.length match
-                case 40 =>
-                  from
-                    .take(10) + "..."
-                case _ =>
-                  from.toString() match
-                    case "playnomm" =>
-                      "010cd45939f064fd82403754bada713e5a9563a1".take(
-                        10,
-                      ) + "..."
-                    case "eth-gateway" =>
-                      "ca79f6fb199218fa681b8f441fefaac2e9a3ead3".take(
-                        10,
-                      ) + "..."
-                    case _ =>
-                      from,
-            ),
-          ),
-          div(`class` := "cell type-3")(
-            span(
-              onClick(
-                PageMsg.PreUpdate(
-                  PageName.AccountDetail(
-                    getOptionValue(
-                      each.toAddr,
-                      "-",
-                    )
-                      .toString(),
-                  ),
-                ),
-              ),
-            )(
-              to.length match
-                case 40 =>
-                  to
-                    .take(10) + "..."
-                case _ =>
-                  to.toString() match
-                    case "playnomm" =>
-                      "010cd45939f064fd82403754bada713e5a9563a1".take(
-                        10,
-                      ) + "..."
-                    case "eth-gateway" =>
-                      "ca79f6fb199218fa681b8f441fefaac2e9a3ead3".take(
-                        10,
-                      ) + "..."
-                    case _ =>
-                      to,
-            ),
+          gen.cell(
+            Cell.TX_HASH(v.txHash),
+            Cell.AGE(v.createdAt),
+            Cell.PlainStr(v.action),
+            Cell.ACCOUNT_HASH(v.fromAddr),
+            Cell.ACCOUNT_HASH(v.toAddr),
           ),
         ),
       )
--- modules/lmscan-frontend/src/main/scala/io/leisuremeta/chain/lmscan/frontend/utils/validation/validCell.scala
+++ modules/lmscan-frontend/src/main/scala/io/leisuremeta/chain/lmscan/frontend/utils/validation/validCell.scala
@@ -7,6 +7,8 @@ enum Cell:
   case AGE(data: Option[Int])                            extends Cell
   case BLOCK_NUMBER(data: (Option[String], Option[Int])) extends Cell
   case BLOCK_HASH(data: Option[String])                  extends Cell
+  case ACCOUNT_HASH(data: Option[String])                extends Cell
+  case TX_HASH(data: Option[String])                     extends Cell
   case PlainInt(data: Option[Int])                       extends Cell
   case PlainStr(data: Option[String])                    extends Cell
   case AAA(data: String)                                 extends Cell
@@ -25,6 +27,24 @@ object W:
 
   def hash(data: Option[Any]) = getOptionValue(data, "-").toString()
 
+  def accountHash(data: Option[String]) =
+    plainStr(data).length match
+      case 40 =>
+        plainStr(data)
+          .take(10) + "..."
+      case _ =>
+        plainStr(data).toString() match
+          case "playnomm" =>
+            "010cd45939f064fd82403754bada713e5a9563a1".take(
+              10,
+            ) + "..."
+          case "eth-gateway" =>
+            "ca79f6fb199218fa681b8f441fefaac2e9a3ead3".take(
+              10,
+            ) + "..."
+          case _ =>
+            plainStr(data)
+
   def hash10(data: Option[Any]) =
     getOptionValue(data, "-").toString().take(10) + "..."
 
@@ -36,7 +56,27 @@ object gen:
     .map(cell =>
       cell match
         case Cell.Head(data) => div(`class` := "cell")(span()(data))
-
+        case Cell.PlainStr(data) =>
+          div(`class` := "cell")(span()(W.plainStr(data)))
+        case Cell.PlainInt(data) =>
+          div(`class` := "cell")(
+            span(
+            )(W.plainInt(data)),
+          )
+        case Cell.ACCOUNT_HASH(data) =>
+          div(`class` := "cell type-3")(
+            span(
+              onClick(
+                PageMsg.PreUpdate(
+                  PageName.AccountDetail(
+                    W.plainStr(data),
+                  ),
+                ),
+              ),
+            )(
+              W.accountHash(data),
+            ),
+          )
         case Cell.AGE(data) =>
           div(`class` := "cell")(
             span(
@@ -77,11 +117,21 @@ object gen:
               dataAttr("tooltip-text", W.plainStr(data)),
             )(W.hash10(data)),
           )
-        case Cell.PlainInt(data) =>
-          div(`class` := "cell")(
+        case Cell.TX_HASH(data) =>
+          div(`class` := "cell type-3")(
             span(
-            )(W.plainInt(data)),
+              onClick(
+                PageMsg.PreUpdate(
+                  PageName.TransactionDetail(
+                    W.plainStr(data),
+                  ),
+                ),
+              ),
+            )(
+              W.plainStr(data),
+            ),
           )
+
         case _ => div(`class` := "cell")(span()("NO - CELL")),
     )
     .toList
