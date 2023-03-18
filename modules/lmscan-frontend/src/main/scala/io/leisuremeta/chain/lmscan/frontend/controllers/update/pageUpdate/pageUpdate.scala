9b72084106bd2fdaa6b6f77d61b390d51d7e3576
G-yhlee <yhleesoft@gmail.com>
2023-03-18 19:38:28 +0900

    add window.history

--- modules/lmscan-frontend/src/main/scala/io/leisuremeta/chain/lmscan/frontend/controllers/update/pageUpdate/pageUpdate.scala
+++ modules/lmscan-frontend/src/main/scala/io/leisuremeta/chain/lmscan/frontend/controllers/update/pageUpdate/pageUpdate.scala
@@ -1,11 +1,31 @@
 package io.leisuremeta.chain.lmscan.frontend
-
+import tyrian.Html.*
+import scala.scalajs.js
+import org.scalajs.dom.window
 import tyrian.*
 import cats.effect.IO
 
 object PageUpdate:
   def update(model: Model): PageMsg => (Model, Cmd[IO, Msg]) =
     case PageMsg.PreUpdate(page: PageName) =>
+      window.history.pushState(
+        // save page to history
+        page.toString(),
+        null,
+        // show url
+        s"${window.location.origin}/"
+          ++
+            page
+              .toString()
+              .replace("Detail", "")
+              .replace("(", "/")
+              .replace(
+                ",",
+                "/",
+              ) // accountdetail(playnomm,1) => account/playnomm/1
+              .replace(")", "")
+              .toLowerCase(),
+      )
       (
         model.copy(
           curPage = page,
