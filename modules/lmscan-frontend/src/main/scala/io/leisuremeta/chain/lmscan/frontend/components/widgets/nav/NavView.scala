ffc7fc3b51c95a7c85e0dabcc5d028e4409ad684
G-yhlee <yhleesoft@gmail.com>
2023-03-18 15:20:05 +0900

    all change
    Signed-off-by: G-yhlee <yhleesoft@gmail.com>

---
+++ modules/lmscan-frontend/src/main/scala/io/leisuremeta/chain/lmscan/frontend/components/widgets/nav/NavView.scala
@@ -0,0 +1,21 @@
+package io.leisuremeta.chain.lmscan.frontend
+
+import tyrian.Html.*
+import tyrian.*
+
+object NavView:
+
+  def view(model: Model): Html[Msg] =
+    nav(`class` := "")(
+      div(id := "title", onClick(PageMsg.PreUpdate(PageName.DashBoard)))(
+        span(id := "head")(img(id := "head-logo")),
+      ),
+      div(
+        id := "buttons",
+      )(
+        button(
+          `class` := s"",
+          onClick(PageMsg.PreUpdate(PageName.DashBoard)),
+        )(span()("Dashboard")),
+      ),
+    )
--- modules/lmscan-frontend/src/main/scala/io/leisuremeta/chain/lmscan/frontend/controllers/view/View.scala
+++ modules/lmscan-frontend/src/main/scala/io/leisuremeta/chain/lmscan/frontend/controllers/view/View.scala
@@ -8,7 +8,7 @@ object View:
   def view(model: Model): Html[Msg] =
     div(
       div(`class` := "main")(
-        // NavView.view(model),
+        NavView.view(model),
         PageView.view(model),
       ),
     )
