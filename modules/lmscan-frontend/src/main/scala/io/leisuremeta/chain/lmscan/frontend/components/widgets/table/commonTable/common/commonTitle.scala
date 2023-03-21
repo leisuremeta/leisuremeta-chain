c78c3c9a3ce4926059581d11bfca5379e8a1b9c8
G-yhlee <yhleesoft@gmail.com>
2023-03-21 11:06:46 +0900

    clean

--- modules/lmscan-frontend/src/main/scala/io/leisuremeta/chain/lmscan/frontend/components/widgets/table/commonTable/common/commonTitle.scala
+++ modules/lmscan-frontend/src/main/scala/io/leisuremeta/chain/lmscan/frontend/components/widgets/table/commonTable/common/commonTitle.scala
@@ -5,46 +5,36 @@ import io.leisuremeta.chain.lmscan.frontend.Builder.getPage
 
 object Title:
   def block = (model: Model) =>
-    div(
-      //   `class` := s"${State.curPage(model, PageName.DashBoard, "_table-title")} table-title ",
-    )(
+    div()(
       div(
         `class` := s"type-1",
       )(span()("Latest Blocks")),
       div(
         `class` := s"type-2",
       )(
-        // {
-        //   getPage(model.observers, model.observerNumber) match
-        //     case PageCase.DashBoard =>
-        //       span(
-        //         onClick(PageMsg.PreUpdate(PageName.Blocks(1))),
-        //       )("More")
-        //     case _ => div()
-        // },
       ),
     )
-//   def tx = (model: Model) =>
-//     div(
-//       `class` := s"${State.curPage(model, PageName.DashBoard, "_table-title")} table-title ",
-//     )(
-//       div(
-//         `class` := s"type-1",
-//       )(span()("Latest transactions")),
-//       div(
-//         `class` := s"type-2",
-//       )(
-//         span(
-//           onClick(PageMsg.PreUpdate(PageName.Transactions(1))),
-//         )("More"),
-//       ),
-//     )
+  // def tx = (model: Model) =>
+  //   div(
+  //     `class` := s"${State.curPage(model, PageName.DashBoard, "_table-title")} table-title ",
+  //   )(
+  //     div(
+  //       `class` := s"type-1",
+  //     )(span()("Latest transactions")),
+  //     div(
+  //       `class` := s"type-2",
+  //     )(
+  //       span(
+  //         onClick(PageMsg.PreUpdate(PageName.Transactions(1))),
+  //       )("More"),
+  //     ),
+  //   )
 
-//   def nft = (model: Model) =>
-//     div(
-//       `class` := s"table-title ",
-//     )(
-//       div(
-//         `class` := s"type-1",
-//       )(span()("Item Activity")),
-//     )
+  // def nft = (model: Model) =>
+  //   div(
+  //     `class` := s"table-title ",
+  //   )(
+  //     div(
+  //       `class` := s"type-1",
+  //     )(span()("Item Activity")),
+  //   )
