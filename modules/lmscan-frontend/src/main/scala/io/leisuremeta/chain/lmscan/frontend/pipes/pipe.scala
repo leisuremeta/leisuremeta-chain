57a886e7fbd8931f58961955a076ee136981a966
G-yhlee <yhleesoft@gmail.com>
2023-03-21 13:19:33 +0900

    add map filter reduce

--- modules/lmscan-frontend/src/main/scala/io/leisuremeta/chain/lmscan/frontend/pipes/pipe.scala
+++ modules/lmscan-frontend/src/main/scala/io/leisuremeta/chain/lmscan/frontend/pipes/pipe.scala
@@ -2,9 +2,10 @@ package io.leisuremeta.chain.lmscan.frontend
 
 object PipePub:
   // [pub]
-  // |> map
-  // |> filter
-  // |> reduce
+  // |> [m1]
+  // |> [m1]
+  // |> <m1>
+  // |> html(<m1>)
 
   def m1     = "a"
   def m1f1   = "a"
