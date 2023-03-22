e8fa669042e2a5a8063f563c2c7cf4fba5229b62
G-yhlee <yhleesoft@gmail.com>
2023-03-22 09:11:49 +0900

    fix

--- modules/lmscan-frontend/src/main/scala/io/leisuremeta/chain/lmscan/frontend/controllers/model/Pubs/PubCase_Pipe.scala
+++ modules/lmscan-frontend/src/main/scala/io/leisuremeta/chain/lmscan/frontend/controllers/model/Pubs/PubCase_Pipe.scala
@@ -6,13 +6,6 @@ import io.leisuremeta.chain.lmscan.common.model.PageResponse
 import io.leisuremeta.chain.lmscan.common.model.BlockInfo
 
 object ReducePipe:
-  // [PubCase]
-  // |> [PubCase_m1] // api 단계에서 처리
-  // |> [PubCase_m2] // parser 로직으로 처리
-  // |> [PubCase_m2f1] // 필터 로직으로 처리
-  // |> <PubCase_m1f1-r> // ReducePipe 로 처리
-  // |> html(<m1>)
-
   def getBlocks = (model: Model) =>
     getPage(model.observers).pubs.reverse
       .filter(d =>
@@ -23,3 +16,10 @@ object ReducePipe:
       .pub_m2
       .payload
       .toList
+
+  // [PubCase]
+  // |> [PubCase_m1] // api 단계에서 처리
+  // |> [PubCase_m2] // parser 로직으로 처리
+  // |> [PubCase_m2f1] // 필터 로직으로 처리
+  // |> <PubCase_m1f1-r> // ReducePipe 로 처리
+  // |> html(<m1>)
