ca1b671d706fa7231de0f3a296aa64b9371f1975
G-yhlee <yhleesoft@gmail.com>
2023-03-23 16:50:32 +0900

    fix block detail page

--- modules/lmscan-frontend/src/main/scala/io/leisuremeta/chain/lmscan/frontend/utils/validation/validCell.scala
+++ modules/lmscan-frontend/src/main/scala/io/leisuremeta/chain/lmscan/frontend/utils/validation/validCell.scala
@@ -196,10 +196,10 @@ object gen:
             span(
               onClick(
                 PageMsg.PreUpdate(
-                  PageCase
-                    .BlockDetail(pubs =
-                      List(PubCase.BlockDetailPub(hash = plainStr(hash))),
-                    ),
+                  PageCase.BlockDetail(
+                    url = s"block/${plainStr(hash)}",
+                    pubs = List(PubCase.BlockDetailPub(hash = plainStr(hash))),
+                  ),
                 ),
               ),
             )(plainLong(number)),
