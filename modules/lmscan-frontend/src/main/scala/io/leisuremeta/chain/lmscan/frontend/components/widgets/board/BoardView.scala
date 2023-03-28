4856c800bd0b83889aedf422208e37c5c6077f93
G-yhlee <yhleesoft@gmail.com>
2023-03-28 10:28:42 +0900

    edit loading

--- modules/lmscan-frontend/src/main/scala/io/leisuremeta/chain/lmscan/frontend/components/widgets/board/BoardView.scala
+++ modules/lmscan-frontend/src/main/scala/io/leisuremeta/chain/lmscan/frontend/components/widgets/board/BoardView.scala
@@ -22,85 +22,78 @@ object BoardView:
 
     div(`class` := "board-area")(
       div(`class` := "board-list x ")(
-        div(`class` := "loader-container")(
-          div(`class` := "board-container xy-center  ")(
-            {
-              data != new SummaryModel match
-                case false => div(`class` := "loader")()
-                case _ =>
-                  div(
-                    `class` := "board-text y-center gap-10px",
-                  )(
-                    div(`class` := "font-16px color-white font-bold")(
-                      Board.LM_Price,
-                    ),
-                    div(`class` := "color-white font-bold")(
-                      plainStr(data.lmPrice).take(6) + " USDT",
-                    ),
-                  )
-            },
-          ),
+        div(`class` := "board-container xy-center position-relative  ")(
+          div(
+            `class` := "board-text y-center gap-10px",
+          )(
+            div(`class` := "font-16px color-white font-bold")(
+              Board.LM_Price,
+            ),
+            div(`class` := "color-white font-bold")(
+              plainStr(data.lmPrice).take(6) + " USDT",
+            ),
+          ), {
+            data != new SummaryModel match
+              case false =>
+                LoaderView.view(model)
+
+              case _ => div()
+          },
         ),
-        div(`class` := "loader-container")(
-          div(`class` := "board-container xy-center  ")(
-            {
-              get_ViewCase(model).blockInfo(0) != new BlockInfo match
-                case false => div(`class` := "loader")()
-                case _ =>
-                  div(
-                    `class` := "board-text y-center gap-10px",
-                  )(
-                    div(`class` := "font-16px color-white font-bold")(
-                      Board.Block_Number,
-                    ),
-                    div(`class` := "color-white font-bold")(
-                      plainStr(
-                        get_ViewCase(model).blockInfo(0).number,
-                      ),
-                    ),
-                  )
-            },
-          ),
+        div(`class` := "board-container xy-center position-relative  ")(
+          div(
+            `class` := "board-text y-center gap-10px",
+          )(
+            div(`class` := "font-16px color-white font-bold")(
+              Board.Block_Number,
+            ),
+            div(`class` := "color-white font-bold")(
+              plainStr(
+                get_ViewCase(model).blockInfo(0).number,
+              ),
+            ),
+          ), {
+            get_ViewCase(model).blockInfo(0) != new BlockInfo match
+              case false =>
+                LoaderView.view(model)
+              case _ => div()
+          },
         ),
       ),
       div(`class` := "board-list x")(
-        div(`class` := "loader-container")(
-          div(`class` := "board-container xy-center  ")(
-            {
-              data != new SummaryModel match
-                case false => div(`class` := "loader")()
-                case _ =>
-                  div(
-                    `class` := "board-text y-center gap-10px",
-                  )(
-                    div(`class` := "font-16px color-white font-bold")(
-                      Board.Transactions,
-                    ),
-                    div(`class` := "color-white font-bold")(
-                      plainStr(data.txCountInLatest24h),
-                    ),
-                  )
-            },
-          ),
+        div(`class` := "board-container xy-center position-relative  ")(
+          div(
+            `class` := "board-text y-center gap-10px",
+          )(
+            div(`class` := "font-16px color-white font-bold")(
+              Board.Transactions,
+            ),
+            div(`class` := "color-white font-bold")(
+              plainStr(data.txCountInLatest24h),
+            ),
+          ), {
+            data != new SummaryModel match
+              case false =>
+                LoaderView.view(model)
+              case _ => div()
+          },
         ),
-        div(`class` := "loader-container")(
-          div(`class` := "board-container xy-center  ")(
-            {
-              data != new SummaryModel match
-                case false => div(`class` := "loader")()
-                case _ =>
-                  div(
-                    `class` := "board-text y-center gap-10px",
-                  )(
-                    div(`class` := "font-16px color-white font-bold")(
-                      Board.Accounts,
-                    ),
-                    div(`class` := "color-white font-bold")(
-                      plainStr(data.totalAccounts),
-                    ),
-                  )
-            },
-          ),
+        div(`class` := "board-container xy-center position-relative  ")(
+          div(
+            `class` := "board-text y-center gap-10px",
+          )(
+            div(`class` := "font-16px color-white font-bold")(
+              Board.Accounts,
+            ),
+            div(`class` := "color-white font-bold")(
+              plainStr(data.totalAccounts),
+            ),
+          ), {
+            data != new SummaryModel match
+              case false =>
+                LoaderView.view(model)
+              case _ => div()
+          },
         ),
       ),
     )
