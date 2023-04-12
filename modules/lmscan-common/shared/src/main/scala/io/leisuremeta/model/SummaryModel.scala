cccf9bf3669a8d125972e1a85dd4f113b6bcf48b
G-yhlee <yhleesoft@gmail.com>
2023-04-12 15:42:06 +0900

    fix summarymodel

--- modules/lmscan-common/shared/src/main/scala/io/leisuremeta/model/SummaryModel.scala
+++ modules/lmscan-common/shared/src/main/scala/io/leisuremeta/model/SummaryModel.scala
@@ -1,10 +1,10 @@
 package io.leisuremeta.chain.lmscan.common.model
 
 final case class SummaryModel(
-  id: Option[Long] = None,
-  lmPrice: Option[Double] = None,
-  blockNumber: Option[Long] = None,
-  txCountInLatest24h: Option[Long] = None,
-  totalAccounts: Option[Long] = None,
-  createdAt: Option[Long] = None,
+    id: Option[Long] = None,
+    lmPrice: Option[Double] = None,
+    blockNumber: Option[Long] = None,
+    totalTxSize: Option[Long] = None,
+    totalAccounts: Option[Long] = None,
+    createdAt: Option[Long] = None,
 )
