d66a2988ce07dd5a808119822cf88bf478f383e0
G-yhlee <yhleesoft@gmail.com>
2023-04-26 11:09:28 +0900

    clean

--- modules/lmscan-frontend/src/main/scala/io/leisuremeta/chain/lmscan/frontend/utils/validation/validPageName.scala
+++ modules/lmscan-frontend/src/main/scala/io/leisuremeta/chain/lmscan/frontend/utils/validation/validPageName.scala
@@ -5,35 +5,7 @@ import io.circe.syntax.*
 import io.circe.parser.*
 import io.leisuremeta.chain.lmscan.frontend.Log.log
 
-// enum PageName:
-//   case DashBoard
-//   case Blocks(page: Int)               extends PageName
-//   case Transactions(page: Int)         extends PageName
-//   case TransactionDetail(hash: String) extends PageName
-//   case BlockDetail(hash: String)       extends PageName
-//   case NftDetail(hash: String)         extends PageName
-//   case AccountDetail(hash: String)     extends PageName
-//   case Page64(hash: String)            extends PageName
-//   case NoPage                          extends PageName
-
 object ValidPageName:
-//   def getPage(search: PageName): PageName =
-//     search match
-//       case PageName.DashBoard            => search
-//       case PageName.Blocks(_)            => search
-//       case PageName.Transactions(_)      => search
-//       case PageName.BlockDetail(_)       => search
-//       case PageName.AccountDetail(_)     => search
-//       case PageName.TransactionDetail(_) => search
-//       case PageName.NftDetail(_)         => search
-//       case _ =>
-//         search.toString().length() match
-//           case 40 => PageName.AccountDetail(search.toString())
-//           case 25 => PageName.NftDetail(search.toString())
-//           case 64 => PageName.Page64(search.toString())
-//           case _ =>
-//             PageName.NoPage
-
   def getPageString(search: String): PageCase =
     search match
       case _ =>
