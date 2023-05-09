1079af19a46c281cd6c810163388779bc0ffed7d
sungkmi <sungkmi@gmail.com>
2023-05-09 12:46:59 +0900

    fix gateway api client to use common thread pool backend

--- modules/eth-gateway-common/src/main/scala/io/leisuremeta/chain/gateway/eth/common/GatewayResource.scala
+++ modules/eth-gateway-common/src/main/scala/io/leisuremeta/chain/gateway/eth/common/GatewayResource.scala
@@ -3,14 +3,18 @@ package io.leisuremeta.chain.gateway.eth.common
 import cats.data.EitherT
 import cats.effect.{Async, Resource}
 
+import org.web3j.protocol.Web3j
 import scodec.bits.ByteVector
 import sttp.client3.*
+import sttp.client3.armeria.cats.ArmeriaCatsBackend
 
 import client.*
 
 object GatewayResource:
-
-  def getAllResource[F[_]: Async](conf: GatewayConf) = for
+  def getAllResource[F[_]: Async](conf: GatewayConf): Resource[
+    EitherT[F, String, *],
+    (GatewayKmsClient[F], Web3j, GatewayDatabaseClient[F], SttpBackend[F, Any]),
+  ] = for
     kms <- GatewayKmsClient
       .make[F](conf.kmsAlias)
       .mapK(EitherT.liftK[F, String])
@@ -18,9 +22,10 @@ object GatewayResource:
       EitherT.fromEither:
         ByteVector.fromHexDescriptive(conf.encryptedEthEndpoint)
     web3jPlaintextResource <- Resource.eval(kms.decrypt(web3jBytes.toArray))
-    web3jPlaintext         <- web3jPlaintextResource.mapK(EitherT.liftK[F, String])
+    web3jPlaintext <- web3jPlaintextResource.mapK(EitherT.liftK[F, String])
     web3jEndpoint = String(web3jPlaintext, "UTF-8")
-    web3j <- GatewayWeb3Service.web3Resource[F](web3jEndpoint)
+    web3j <- GatewayWeb3Service
+      .web3Resource[F](web3jEndpoint)
       .mapK(EitherT.liftK[F, String])
     dbBytes <- Resource.eval:
       EitherT.fromEither:
@@ -31,7 +36,5 @@ object GatewayResource:
     db <- GatewayDatabaseClient
       .make[F](dbEndpoint, conf.databaseTableName, conf.databaseValueColumn)
       .mapK(EitherT.liftK[F, String])
-    api <- GatewayApiClient
-      .make[F](uri"${conf.lmEndpoint}")
-      .mapK(EitherT.liftK[F, String])
-  yield (kms, web3j, db, api)
+    sttp <- ArmeriaCatsBackend.resource[F]().mapK(EitherT.liftK[F, String])
+  yield (kms, web3j, db, sttp)
--- modules/eth-gateway-common/src/main/scala/io/leisuremeta/chain/gateway/eth/common/client/GatewayApiClient.scala
+++ modules/eth-gateway-common/src/main/scala/io/leisuremeta/chain/gateway/eth/common/client/GatewayApiClient.scala
@@ -6,7 +6,7 @@ import cats.effect.{Async, Resource}
 import cats.syntax.bifunctor.*
 import cats.syntax.functor.*
 
-import sttp.client3.armeria.cats.ArmeriaCatsBackend
+import sttp.client3.SttpBackend
 import sttp.model.Uri
 import sttp.tapir.DecodeResult
 import sttp.tapir.client.sttp.SttpClientInterpreter
@@ -21,30 +21,30 @@ object GatewayApiClient:
 
   def apply[F[_]: GatewayApiClient]: GatewayApiClient[F] = summon
 
-  def make[F[_]: Async](uri: Uri): Resource[F, GatewayApiClient[F]] =
-    ArmeriaCatsBackend
-      .resource[F]()
-      .map: backend =>
-        val sttpClient = SttpClientInterpreter().toClient(
-          GatewayApi.postDecryptEndpoint,
-          Some(uri),
-          backend,
-        )
-        def sanitize[A, B](
-            result: DecodeResult[Either[A, B]],
-        ): Either[String, B] =
-          result match
-            case DecodeResult.Value(v)   => v.leftMap(_.toString)
-            case f: DecodeResult.Failure => Left(s"Fail: ${f.toString()}")
+  def make[F[_]: Async](
+    backend: SttpBackend[F, Any],
+    uri: Uri
+  ): GatewayApiClient[F] =
+    val sttpClient = SttpClientInterpreter().toClient(
+      GatewayApi.postDecryptEndpoint,
+      Some(uri),
+      backend,
+    )
+    def sanitize[A, B](
+        result: DecodeResult[Either[A, B]],
+    ): Either[String, B] =
+      result match
+        case DecodeResult.Value(v)   => v.leftMap(_.toString)
+        case f: DecodeResult.Failure => Left(s"Fail: ${f.toString()}")
 
-        new GatewayApiClient[F]:
-          override def get(
-              key: String,
-              doublyEncryptedFrontPartBase64: String,
-          ): EitherT[F, String, String] =
-            EitherT
-              .apply:
-                sttpClient(
-                  GatewayApi.GatewayRequest(key, doublyEncryptedFrontPartBase64),
-                ).map(sanitize)
-              .map(_.singlyEncryptedBase64)
+    new GatewayApiClient[F]:
+      override def get(
+          key: String,
+          doublyEncryptedFrontPartBase64: String,
+      ): EitherT[F, String, String] =
+        EitherT
+          .apply:
+            sttpClient(
+              GatewayApi.GatewayRequest(key, doublyEncryptedFrontPartBase64),
+            ).map(sanitize)
+          .map(_.singlyEncryptedBase64)
