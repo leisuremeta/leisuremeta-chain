fa304a91fc3451635e32d7f804e958b7725fcb09
sungkmi <sungkmi@gmail.com>
2023-05-27 00:46:52 +0900

    fix circe codec of StateRoot

--- modules/api/src/main/scala/io/leisuremeta/chain/api/model/StateRoot.scala
+++ modules/api/src/main/scala/io/leisuremeta/chain/api/model/StateRoot.scala
@@ -11,6 +11,7 @@ import sttp.tapir.*
 
 import lib.codec.byte.{ByteDecoder, ByteEncoder}
 import lib.crypto.Hash
+import lib.datatype.UInt256
 import lib.merkle.MerkleTrieNode.MerkleRoot
 import account.EthAddress
 import reward.*
@@ -32,8 +33,10 @@ object StateRoot:
   given byteEncoder: ByteEncoder[StateRoot] =
     ByteEncoder.optionByteEncoder[MerkleRoot]
 
-  given circeDecoder: Decoder[StateRoot] = deriveDecoder[Option[MerkleRoot]]
-  given circeEncoder: Encoder[StateRoot] = deriveEncoder[Option[MerkleRoot]]
+  given circeDecoder: Decoder[StateRoot] = 
+    Decoder.decodeOption[MerkleRoot]
+  given circeEncoder: Encoder[StateRoot] =
+    Encoder.encodeOption[MerkleRoot]
 
   given tapirSchema: Schema[StateRoot] = Schema.string
   given eqStateRoot: Eq[StateRoot]     = Eq.fromUniversalEquals
