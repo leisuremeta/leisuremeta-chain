1ff94f036e21f00916330bf6caf7ccd57897f08c
tong92 <tong92power@gmail.com>
2023-06-01 11:36:41 +0900

    Update db connection config
    Update dbEndpoint result
    Change gateway db connector

--- modules/eth-gateway-common/src/main/scala/io/leisuremeta/chain/gateway/eth/common/client/GatewayDatabaseClient.scala
+++ modules/eth-gateway-common/src/main/scala/io/leisuremeta/chain/gateway/eth/common/client/GatewayDatabaseClient.scala
@@ -8,6 +8,7 @@ import cats.syntax.functor.*
 
 import com.github.jasync.sql.db.QueryResult
 import com.github.jasync.sql.db.mysql.MySQLConnectionBuilder
+import com.github.jasync.sql.db.ConnectionPoolConfigurationBuilder
 
 trait GatewayDatabaseClient[F[_]]:
   def select(
@@ -18,6 +19,7 @@ object GatewayDatabaseClient:
 
   def apply[F[_]: GatewayDatabaseClient]: GatewayDatabaseClient[F] = summon
 
+  @SuppressWarnings(Array("org.wartremover.warts.OptionPartial"))
   def make[F[_]: Async](
       dbEndpoint: String,
       tableName: String,
@@ -26,7 +28,17 @@ object GatewayDatabaseClient:
     Resource
       .make:
         Async[F].blocking:
-          MySQLConnectionBuilder.createConnectionPool(dbEndpoint)
+          val reg = """^jdbc:mysql://(.*):(\d{4,5})/(.*)\?user=(.*)&password=(.*)$""".r
+          val builder = for 
+            res <- reg.findFirstMatchIn(dbEndpoint)
+            config = ConnectionPoolConfigurationBuilder()
+            _ = config.setHost(res.group(1))
+            _ = config.setPort(res.group(2).toInt)
+            _ = config.setDatabase(res.group(3))
+            _ = config.setUsername(res.group(4))
+            _ = config.setPassword(res.group(5))
+          yield config
+          MySQLConnectionBuilder.createConnectionPool(builder.get)
       .apply: connection =>
         Async[F]
           .fromCompletableFuture:
