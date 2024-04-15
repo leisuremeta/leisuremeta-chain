db2b47a0df1a9b86732ac7d9e25998920ca4b788
sungkmi <sungkmi@gmail.com>
2024-04-15 14:23:18 +0900

    change archive format to add block number and tx hash

--- modules/archive/src/main/scala/io/leisuremeta/chain/archive/ArchiveMain.scala
+++ modules/archive/src/main/scala/io/leisuremeta/chain/archive/ArchiveMain.scala
@@ -29,7 +29,6 @@ import api.model.*
 import lib.crypto.{Hash, Signature}
 import lib.datatype.*
 
-
 final case class PBlock(
     header: PHeader,
     transactionHashes: Set[Signed.TxHash],
@@ -49,88 +48,110 @@ object ArchiveMain extends IOApp:
 
   val archiveFileName = "txs1.archive"
 
-  val backend = ArmeriaCatsBackend[IO]()
-
-  def logTxs(txs: String): IO[Unit] = IO.blocking {
+  def logTxs(contents: String): IO[Unit] = IO.blocking:
     val path = Paths.get(archiveFileName)
-    val _ = Files.write(path, txs.getBytes, StandardOpenOption.CREATE, StandardOpenOption.WRITE, StandardOpenOption.APPEND)
-  }
+    val _ = Files.write(
+      path,
+      contents.getBytes,
+      StandardOpenOption.CREATE,
+      StandardOpenOption.WRITE,
+      StandardOpenOption.APPEND,
+    )
 
-  def get[F[_]: Monad, A: io.circe.Decoder](backend: SttpBackend[F, Any])(uri: Uri): EitherT[F, String, A] =
-    EitherT{
-      basicRequest
-        .get(uri)
-        .send(backend)
-        .map{ response =>
-          val result = for
-            body <- response.body
-            a <- decode[A](body).leftMap(_.getMessage())
-          yield a
+  def get[F[_]: Monad, A: io.circe.Decoder](
+      backend: SttpBackend[F, Any],
+  )(uri: Uri): EitherT[F, String, A] = EitherT:
+    basicRequest
+      .get(uri)
+      .send(backend)
+      .map: response =>
+        for
+          body <- response.body
+          a    <- decode[A](body).leftMap(_.getMessage())
+        yield a
 
-          result
-        }
-    }
-
-  def put[F[_]: Monad](backend: SttpBackend[F, Any])(uri: Uri)(line: String): EitherT[F, String, List[Signed.TxHash]] =
-    EitherT{
+  def put[F[_]: Monad](
+      backend: SttpBackend[F, Any],
+  )(uri: Uri)(line: String): EitherT[F, String, List[Signed.TxHash]] =
+    EitherT:
 //      println(s"Req: $line")
 
       basicRequest
         .post(uri)
         .body(line)
         .send(backend)
-        .map{ response =>
+        .map: response =>
 //          println(s"Response: $response")
           val result = for
             body <- response.body
-            a <- decode[List[Signed.TxHash]](body).leftMap(_.getMessage())
+            a    <- decode[List[Signed.TxHash]](body).leftMap(_.getMessage())
           yield a
 
           result
-        }
-    }
 
-  def getTransaciton[F[_]: Monad](backend: SttpBackend[F, Any])(txHash: Signed.TxHash): EitherT[F, String, TransactionWithResult] =
-    get[F, TransactionWithResult](backend){
-      uri"$baseUri/tx/${txHash.toUInt256Bytes.toBytes.toHex}"
-    }.leftMap{ msg =>
-      scribe.error(s"error msg: $msg")
-      msg
-    }
+  def getTransaction[F[_]: Monad](
+      backend: SttpBackend[F, Any],
+  )(txHash: Signed.TxHash): EitherT[F, String, TransactionWithResult] =
+    get[F, TransactionWithResult](backend)
+      .apply:
+        uri"$baseUri/tx/${txHash.toUInt256Bytes.toBytes.toHex}"
+      .leftMap: msg =>
+        scribe.error(s"error msg: $msg")
+        msg
 
-  def getBlock[F[_]: Monad](backend: SttpBackend[F, Any])(blockHash: Block.BlockHash): EitherT[F, String, PBlock] =
-    get[F, PBlock](backend){
+  def getBlock[F[_]: Monad](
+      backend: SttpBackend[F, Any],
+  )(blockHash: Block.BlockHash): EitherT[F, String, PBlock] =
+    get[F, PBlock](backend):
       uri"$baseUri/block/${blockHash.toUInt256Bytes.toBytes.toHex}"
-    }
-  
-  def getStatus[F[_]: Monad](backend: SttpBackend[F, Any]): EitherT[F, String, NodeStatus] =
-    get[F, NodeStatus](backend){
-      uri"$baseUri/status"
-    }
 
-  def loop[F[_]: Monad](backend: SttpBackend[F, Any])(next: Block.BlockHash, genesis: Block.BlockHash, count: Long)(run: Set[Signed.TxHash] => EitherT[F, String, Unit]): EitherT[F, String, Long] =
-    for
-      block <- getBlock[F](backend)(next)
-      _ <- EitherT.pure(scribe.info(s"block ${block.header.number}: $next"))
-      _ <- run(block.transactionHashes).recover(_ => ())
-      count1 <- loop[F](backend)(block.header.parentHash, genesis, count + 1)(run)
-    yield count1
-    
+  def getStatus[F[_]: Monad](
+      backend: SttpBackend[F, Any],
+  ): EitherT[F, String, NodeStatus] =
+    get[F, NodeStatus](backend)(uri"$baseUri/status")
+
+  def loop[F[_]: Monad](
+      backend: SttpBackend[F, Any],
+  )(next: Block.BlockHash, genesis: Block.BlockHash, count: Long)(
+      run: (
+          BigNat,
+          Block.BlockHash,
+          Set[Signed.TxHash],
+      ) => EitherT[F, String, Unit],
+  ): EitherT[F, String, Long] = for
+    block <- getBlock[F](backend)(next)
+    _     <- EitherT.pure(scribe.info(s"block ${block.header.number}: $next"))
+    _ <- run(block.header.number, next, block.transactionHashes).recover: msg =>
+      scribe.error(s"error msg: $msg")
+      ()
+    count1 <- loop[F](backend)(block.header.parentHash, genesis, count + 1)(run)
+  yield count1
+
   def run(args: List[String]): IO[ExitCode] =
-    for _ <- ArmeriaCatsBackend.resource[IO](
-      SttpBackendOptions.Default.connectionTimeout(10.minutes)
-    ).use { backend => {
-        for
-          status <- getStatus[IO](backend)
-          block <- getBlock[IO](backend)(status.bestHash)
-          count <- loop[IO](backend)(status.bestHash, status.genesisHash, 0){ txSet =>
-            for
-              txs <- txSet.toList.traverse{ getTransaciton[IO](backend) }
-              _ <- EitherT.right(logTxs(txs.map(_.signedTx).asJson.noSpaces + "\n"))
-            yield ()
-          }
-        yield
-          println(s"total number of block: $count")
+    for _ <- ArmeriaCatsBackend
+        .resource[IO]:
+          SttpBackendOptions.Default.connectionTimeout(10.minutes)
+        .use: backend =>
+          val program = for
+            status <- getStatus[IO](backend)
+            block  <- getBlock[IO](backend)(status.bestHash)
+            count <- loop[IO](backend)(
+              status.bestHash,
+              status.genesisHash,
+              0,
+            ): (blockNumber, blockHash, txSet) =>
+              txSet.toList
+                .sortBy(_.toUInt256Bytes.toHex)
+                .traverse: txHash =>
+                  for
+                    tx <- getTransaction[IO](backend)(txHash)
+                    txString = tx.signedTx.asJson.noSpaces
+                    _ <- EitherT.right:
+                      logTxs:
+                        s"$blockNumber\t${txHash.toUInt256Bytes.toHex}\t$txString\n"
+                  yield ()
+                .as(())
+          yield println(s"total number of block: $count")
 
 //        val from = 0
 //        val to = 1000000
@@ -144,6 +165,5 @@ object ArchiveMain extends IOApp:
 //                Nil
 //            }
 //        }
-      }.value
-    }
+          program.value
     yield ExitCode.Success
