fa9963de88233ce9e151a780734588b3b848b54f
sungkmi <sungkmi@gmail.com>
2024-06-26 11:03:07 +0900

    implement to handle CreateVoteProposal tx

--- modules/node/src/main/scala/io/leisuremeta/chain/node/dapp/submodule/PlayNommDAppVoting.scala
+++ modules/node/src/main/scala/io/leisuremeta/chain/node/dapp/submodule/PlayNommDAppVoting.scala
@@ -6,17 +6,18 @@ package submodule
 //import cats.Monad
 import cats.data.{EitherT, StateT}
 import cats.effect.Concurrent
-//import cats.syntax.all.*
+import cats.syntax.all.*
 
 import api.model.{
 //  Account,
   AccountSignature,
-//  Signed,
+  Signed,
   Transaction,
   TransactionWithResult,
 }
 //import api.model.TransactionWithResult.ops.*
 //import api.model.token.*
+import api.model.voting.*
 //import api.model.token.SnapshotState.SnapshotId.*
 //import lib.codec.byte.ByteEncoder.ops.*
 //import lib.crypto.Hash
@@ -36,9 +37,31 @@ object PlayNommDAppVoting:
     TransactionWithResult,
   ] = tx match
     case cp: Transaction.VotingTx.CreateVoteProposal =>
-      ???
+      for
+        _ <- PlayNommDAppAccount.verifySignature(sig, tx)
+        _ <- cp.votingTokens.toList.traverse: defId =>
+          PlayNommDAppToken.checkMinterAndGetTokenDefinition(sig.account, defId)
+        proposal = Proposal(
+          createdAt = cp.createdAt,
+          proposalId = cp.proposalId,
+          title = cp.title,
+          description = cp.description,
+          votingTokens = cp.votingTokens,
+          snapshotId = cp.snapshotId,
+          voteStart = cp.voteStart,
+          voteEnd = cp.voteEnd,
+          voteOptions = cp.voteOptions,
+          quorum = cp.quorum,
+          passThresholdNumer = cp.passThresholdNumer,
+          passThresholdDenom = cp.passThresholdDenom,
+        )
+        _ <- PlayNommState[F].voting.proposal
+          .put(cp.proposalId, proposal)
+          .mapK:
+            PlayNommDAppFailure.mapInternal:
+              s"Failed to save proposal: ${cp.proposalId}"
+      yield TransactionWithResult(Signed(sig, cp))(None)
     case cv: Transaction.VotingTx.CastVote =>
       ???
     case tv: Transaction.VotingTx.TallyVotes =>
       ???
-
