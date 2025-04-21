62a89d9eb066d1ce68974cddc18e863095ed8a89
tong92 <tong92power@gmail.com>
2025-04-21 13:22:21 +0900

    Update lm discord url

--- modules/lmscan-frontend/src/main/scala/io/leisuremeta/chain/lmscan/frontend/components/Footer.scala
+++ modules/lmscan-frontend/src/main/scala/io/leisuremeta/chain/lmscan/frontend/components/Footer.scala
@@ -8,60 +8,60 @@ object Footer:
       div(cls := "footer-left")(
         p(
           strong("LMSCAN"),
-          span("for LeisureMeta Chain")
+          span("for LeisureMeta Chain"),
         ),
-       strong("Copyright © LM LLC All Rights Reserved")
+        strong("Copyright © LM LLC All Rights Reserved"),
       ),
       div(cls := "footer-right")(
         dl(
           dt("Company Family"),
           dd(
             a(
-              href := "https://themoonlabs.net/",
-              target :="_blank",
-              cls := "icon-The-Moon-Labs"
+              href   := "https://themoonlabs.net/",
+              target := "_blank",
+              cls    := "icon-The-Moon-Labs",
             )("The Moon Labs"),
             a(
-              href := "https://leisuremeta.io/",
-              target :="_blank",
-              cls := "icon-leisuremetaverse"
+              href   := "https://leisuremeta.io/",
+              target := "_blank",
+              cls    := "icon-leisuremetaverse",
             )("LeisureMetaverse"),
             a(
-              href := "https://www.ilikelm.com/",
-              target :="_blank",
-              cls := "icon-lm-nova"
+              href   := "https://www.ilikelm.com/",
+              target := "_blank",
+              cls    := "icon-lm-nova",
             )("ILIKELM"),
             a(
-              href := "https://www.themoonent.com/",
-              target :="_blank",
-              cls := "icon-tme"
+              href   := "https://www.themoonent.com/",
+              target := "_blank",
+              cls    := "icon-tme",
             )("THE MOON ENTERTAINMENT"),
-          )
+          ),
         ),
         dl(
           dt("Social"),
           dd(
             a(
-              href := "https://github.com/leisuremeta/leisuremeta-chain",
-              target :="_blank",
-              cls := "icon-github"
+              href   := "https://github.com/leisuremeta/leisuremeta-chain",
+              target := "_blank",
+              cls    := "icon-github",
             )("Github"),
             a(
-              href := "https://t.me/LeisureMeta_Official",
-              target :="_blank",
-              cls := "icon-telegram"
+              href   := "https://t.me/LeisureMeta_Official",
+              target := "_blank",
+              cls    := "icon-telegram",
             )("Telegram"),
             a(
-              href := "https://twitter.com/LeisureMeta_LM",
-              target :="_blank",
-              cls := "icon-twitter"
+              href   := "https://twitter.com/LeisureMeta_LM",
+              target := "_blank",
+              cls    := "icon-twitter",
             )("Twitter"),
             a(
-              href := "https://discord.com/invite/5ttwp2dzEr",
-              target :="_blank",
-              cls := "icon-discord"
+              href   := "https://discord.gg/leisuremetaofficial",
+              target := "_blank",
+              cls    := "icon-discord",
             )("Discord"),
-          )
+          ),
         ),
       ),
     )
