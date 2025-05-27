cd34c712cd9d4e8eeac6128913a6c132b9381af8
tong92 <tong92power@gmail.com>
2025-05-27 17:55:45 +0900

    Update agent main

--- modules/lmscan-agent/src/main/scala/io/leisuremeta/chain/lmscan/agent/ScanAgentMain.scala
+++ modules/lmscan-agent/src/main/scala/io/leisuremeta/chain/lmscan/agent/ScanAgentMain.scala
@@ -1,18 +1,38 @@
-package io.leisuremeta.chain.lmscan.agent
+package io.leisuremeta.chain
+package lmscan.agent
 
-import cats.effect._
+import cats.effect.*
+import service.RequestService
+import cats.data.EitherT
+import io.leisuremeta.chain.lmscan.agent.service.StoreService
+import scribe.file.*
+import scribe.format.*
+import io.leisuremeta.chain.lmscan.agent.apps.SummaryStoreApp
 
 object ScanAgentMain extends IOApp:
-  def build() =
-    given PrintGreet()
-    val program = for
-      a <- (Korean.gen().run.foreverM).start
-      b <- (English.gen().run.foreverM).start
-      x <- a.join
-      y <- b.join
-    yield (x, y)
-    program.toResource
-
-  def run(args: List[String]): IO[ExitCode] = 
-    val p = build()
-    p.onCancel(IO.println("End").toResource).useForever
+  scribe.Logger.root
+    .withHandler(
+      writer = FileWriter(
+        "logs" / ("agent-" % year % "-" % month % "-" % day % ".log"),
+      ),
+      formatter =
+        formatter"[$threadName] $positionAbbreviated - $messages$newLine",
+      minimumLevel = Some(scribe.Level.Error),
+    )
+    .replace()
+  def run(args: List[String]): IO[ExitCode] =
+    val conf = ScanAgentConfig.load
+    ScanAgentResource
+      .build[IO](conf)
+      .use: (post, sqlite, server) =>
+        val client = RequestService.build[IO](server)
+        val remote = StoreService.buildRemote(post)
+        val local  = StoreService.buildLocal(sqlite)
+        val summary = SummaryStoreApp.build[IO](conf.market, conf.es)(
+          remote,
+          client,
+        )
+        EitherT.liftF:
+          summary.run &> LoopCheckerApp.run(remote, local, client, conf.base)
+      .value
+      .as(ExitCode.Success)
